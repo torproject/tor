cd756db2654580aae12946dbf1f04929c0daa18d
Nick Mathewson <nickm@torproject.org>
2003-04-07 13:25:44 +0000

    First test added
    svn:r226

--- trunk/src/or/test.c
+++ trunk/src/or/test.c
@@ -2,7 +2,31 @@
 /* See LICENSE for licensing information */
 /* $Id$ */
 
+#include "or.h"
+#include "../common/test.h"
+
+void
+test_buffers() {
+  char *buf;
+  int buflen, buf_datalen;
+
+  if (buf_new(&buf, &buflen, &buf_datalen)) {
+    test_fail();
+  }
+
+  test_eq(buflen, MAX_BUF_SIZE);
+  test_eq(buf_datalen, 0);
+
+  
+
+  buf_free(buf);
+}
+
+
 int main(int c, char**v) {
+  test_buffers();
+
+  printf("\n");
   return 0;
 }
 
