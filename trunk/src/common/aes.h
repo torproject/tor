2570b70e50f5651f644c2e28e4f6fac4b138f3d3
Nick Mathewson <nickm@torproject.org>
2003-06-30 19:18:12 +0000

    Add a the public-domain AES implementation, with a minimal counter-mode wrapper.
    svn:r361

---
+++ trunk/src/common/aes.h
@@ -0,0 +1,23 @@
+/* Copyright 2003 Roger Dingledine */
+/* See LICENSE for licensing information */
+/* $Id$ */
+
+/* Implements a minimal interface to counter-mode AES. */
+
+#ifndef __AES_H
+#define __AES_H
+
+#include <stdint.h>
+
+struct aes_cnt_cipher;
+typedef struct aes_cnt_cipher aes_cnt_cipher_t;
+
+aes_cnt_cipher_t* aes_new_cipher();
+void aes_free_cipher(aes_cnt_cipher_t *cipher);
+void aes_set_key(aes_cnt_cipher_t *cipher, unsigned char *key, int key_bits);
+void aes_crypt(aes_cnt_cipher_t *cipher, char *input, int len, char *output);
+uint64_t aes_get_counter(aes_cnt_cipher_t *cipher);
+void aes_set_counter(aes_cnt_cipher_t *cipher, uint64_t counter);
+void aes_adjust_counter(aes_cnt_cipher_t *cipher, long delta);
+
+#endif
