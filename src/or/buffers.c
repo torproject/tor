99035f35205734b6c1736fe3aa5a12cb8beaca60
Roger Dingledine <arma@torproject.org>
2003-09-05 11:25:24 +0000

    clean read_to_buf more
    svn:r428

--- src/or/buffers.c
+++ src/or/buffers.c
@@ -50,18 +50,6 @@ int read_to_buf(int s, int at_most, char **buf, int *buflen, int *buf_datalen, i
   if(at_most == 0)
     return 0; /* we shouldn't read anything */
 
-  if(!options.LinkPadding && at_most > 10*sizeof(cell_t)) {
-    /* if no linkpadding: do a rudimentary round-robin so one
-     * connection can't hog a thickpipe
-     */
-    at_most = 10*(CELL_PAYLOAD_SIZE - RELAY_HEADER_SIZE);
-    /* XXX this still isn't perfect. now we read 10 relay data payloads per read --
-     * but if we're reading from a connection that speaks cells, we always
-     * read a partial cell from the network and can't process it yet. Good
-     * enough for now though. (And maybe best, to stress our code more.)
-     */
-  }
-
 //  log_fn(LOG_DEBUG,"reading at most %d bytes.",at_most);
   read_result = read(s, *buf+*buf_datalen, at_most);
   if (read_result < 0) {
