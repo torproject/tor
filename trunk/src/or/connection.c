ed3de79399038a7857cc449d8a5c57217fdbea32
Roger Dingledine <arma@torproject.org>
2004-02-27 23:30:27 +0000

    and another one
    svn:r1147

--- trunk/src/or/connection.c
+++ trunk/src/or/connection.c
@@ -737,6 +737,10 @@ int connection_send_destroy(uint16_t circ_id, connection_t *conn) {
   if(!connection_speaks_cells(conn)) {
      log_fn(LOG_INFO,"CircID %d: At an edge. Marking connection for close.",
             circ_id);
+     conn->has_sent_end = 1; /* we're closing the circuit, nothing to send to */
+/* XXX really, we should separate this function into two functions.
+ * one of them actually sends the destroy cell, as this function's name
+ * implies, and another one destroys a stream. Yes/no? -RD */
      connection_mark_for_close(conn, END_STREAM_REASON_DESTROY);
      return 0;
   }
