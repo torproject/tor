e9b38b4e1857bbde283aea4be314328ec36177ab
Roger Dingledine <arma@torproject.org>
2004-03-12 08:16:48 +0000

    more details when a conn is closed before it's done flushing
    svn:r1262

--- trunk/src/or/main.c
+++ trunk/src/or/main.c
@@ -260,8 +260,10 @@ static void conn_close_if_marked(int i) {
       return;
     }
     if(connection_wants_to_flush(conn)) {
-      log_fn(LOG_WARN,"Conn (fd %d) still wants to flush. Losing %d bytes!",
-             conn->s, (int)buf_datalen(conn->outbuf));
+      log_fn(LOG_WARN,"Conn (fd %d, type %s, state %d) still wants to flush. Losing %d bytes! (Marked at %s:%d)",
+             conn->s, CONN_TYPE_TO_STRING(conn->type), conn->state,
+             (int)buf_datalen(conn->outbuf), conn->marked_for_close_file,
+             conn->marked_for_close);
     }
   }
   connection_remove(conn);
