62d838b5802b493f8ba1a97878befb8994887891
Peter Palfrader <peter@palfrader.org>
2004-03-29 22:09:11 +0000

    Remove automake files from cvs.  Let's see whether it works for Roger too.
    svn:r1369

--- trunk/autogen.sh
+++ trunk/autogen.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
-aclocal && autoheader && autoconf && automake
+aclocal &&
+	autoheader &&
+	autoconf &&
+	automake --add-missing --copy
 ./configure
 
