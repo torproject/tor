1de5f2ea47916a9a114649a97e048eac7f2a2e5c
Nick Mathewson <nickm@torproject.org>
2004-04-01 04:07:09 +0000

    Add some hooks to handle rendezvous-related cells at ORs
    svn:r1429

---
+++ trunk/src/or/rendmid.c
@@ -0,0 +1,37 @@
+/* Copyright 2004 Roger Dingledine */
+/* See LICENSE for licensing information */
+/* $Id$ */
+
+#include "or.h"
+
+int
+rend_mid_establish_intro(circuit_t *circ, char *request, int request_len)
+{
+  return 0;
+}
+
+int
+rend_mid_introduce(circuit_t *circ, char *request, int request_len)
+{
+  return 0;
+}
+
+int
+rend_mid_establish_rendezvous(circuit_t *circ, char *request, int request_len)
+{
+  return 0;
+}
+
+int
+rend_mid_rendezvous(circuit_t *circ, char *request, int request_len)
+{
+  return 0;
+}
+
+/*
+  Local Variables:
+  mode:c
+  indent-tabs-mode:nil
+  c-basic-offset:2
+  End:
+*/
