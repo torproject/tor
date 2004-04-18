7a7baf0552c39414426240c01f02b6dd7dde1326
Roger Dingledine <arma@torproject.org>
2004-04-18 06:35:31 +0000

    expire dir connections that live for more than 5 minutes
    svn:r1663

--- src/or/main.c
+++ src/or/main.c
@@ -306,6 +306,13 @@ static void run_connection_housekeeping(int i, time_t now) {
   cell_t cell;
   connection_t *conn = connection_array[i];
 
+  if(conn->type == CONN_TYPE_DIR &&
+     conn->timestamp_created + 5*60 > now) {
+    log_fn(LOG_INFO,"Expiring wedged directory conn (purpose %d)", conn->purpose);
+    connection_mark_for_close(conn,0);
+    return;
+  }
+
   /* check connections to see whether we should send a keepalive, expire, or wait */
   if(!connection_speaks_cells(conn))
     return;
