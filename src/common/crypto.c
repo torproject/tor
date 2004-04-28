5d1510883ea583b12b1d66156c31f03700c79245
Roger Dingledine <arma@torproject.org>
2004-04-28 20:22:37 +0000

    use nick's _ARRAYSIZE abstraction
    svn:r1741

--- src/common/crypto.c
+++ src/common/crypto.c
@@ -78,8 +78,7 @@ struct crypto_pk_env_t
 struct crypto_cipher_env_t
 {
   unsigned char key[CIPHER_KEY_LEN];
-  unsigned char iv[CIPHER_IV_LEN+1];
-    /* +1 because some compilers don't like a length of 0 */
+  unsigned char iv[_ARRAYSIZE(CIPHER_IV_LEN)];
   aes_cnt_cipher_t *cipher;
 };
 
