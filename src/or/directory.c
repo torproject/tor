d4efa475a1cec096d11d63b28456f0220c719aef
Roger Dingledine <arma@torproject.org>
2004-05-06 22:47:48 +0000

    forward-port header-length-increase patch
    svn:r1814

--- src/or/directory.c
+++ src/or/directory.c
@@ -21,7 +21,7 @@ char rend_publish_string[] = "/rendezvous/publish";
 /* Prefix for downloading rendezvous descriptors. */
 char rend_fetch_url[] = "/rendezvous/";
 
-#define MAX_HEADERS_SIZE 2048
+#define MAX_HEADERS_SIZE 10000
 #define MAX_BODY_SIZE 500000
 
 /********* END VARIABLES ************/
