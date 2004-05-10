9968f1da984bc35cd1b7070fc05f2e5392613a2c
Roger Dingledine <arma@torproject.org>
2004-05-10 02:43:41 +0000

    doxygen for tor_main.c
    svn:r1828

--- src/or/tor_main.c
+++ src/or/tor_main.c
@@ -1,14 +1,17 @@
-/* Copyright 2001,2002,2003 Roger Dingledine, Matej Pfajfar. */
+/* Copyright 2001-2004 Roger Dingledine. */
 /* See LICENSE for licensing information */
 /* $Id$ */
 
-/*****
- * tor_main.c: Entry point for tor binary.  (We keep main() in a
- * separate file so that our unit tests can use functions from main.c)
- *****/
+/**
+ * \file tor_main.c
+ * \brief Entry point for tor binary.
+ **/
 
 int tor_main(int argc, char *argv[]);
 
+/** We keep main() in a separate file so that our unit tests can use
+ * functions from main.c)
+ */
 int main(int argc, char *argv[])
 {
   return tor_main(argc, argv);
