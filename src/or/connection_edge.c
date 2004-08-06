f8c14e5dce8b57dd40ad331e86ad9feab9f46c8c
Roger Dingledine <arma@torproject.org>
2004-08-06 10:12:55 +0000

    make explicit that 'no socks policy' means 'accept'
    svn:r2158

--- src/or/connection_edge.c
+++ src/or/connection_edge.c
@@ -985,6 +985,8 @@ int socks_policy_permits_address(uint32_t addr)
   if (options.SocksPolicy && !socks_policy)
     parse_socks_policy();
 
+  if(!socks_policy)
+    return 1;
   a = router_compare_addr_to_exit_policy(addr, 1, socks_policy);
   if (a==-1)
     return 0;
