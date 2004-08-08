b2c7b5adfb5f085c298d32b67727f70720b2ac2b
Roger Dingledine <arma@torproject.org>
2004-08-08 10:32:36 +0000

    fix a race condition in 008pre2: don't try to extend onto a connection that's still handshaking.
    for servers in clique mode, require the conn to be open before you'll
    choose it for your path.
    
    
    svn:r2198

--- src/or/routerlist.c
+++ src/or/routerlist.c
@@ -192,11 +192,17 @@ void router_add_running_routers_to_smartlist(smartlist_t *sl) {
   for(i=0;i<smartlist_len(routerlist->routers);i++) {
     router = smartlist_get(routerlist->routers, i);
     /* XXX008 for now, only choose verified routers */
-    if(router->is_running && router->is_verified &&
-       (!clique_mode() ||
-        connection_get_by_identity_digest(router->identity_digest,
-                                          CONN_TYPE_OR)))
-      smartlist_add(sl, router);
+    if(router->is_running && router->is_verified) {
+      if(!clique_mode()) {
+        smartlist_add(sl, router);
+      } else {
+        connection_t *conn =
+          connection_get_by_identity_digest(router->identity_digest,
+                                            CONN_TYPE_OR);
+        if(conn && conn->state == OR_CONN_STATE_OPEN)
+          smartlist_add(sl, router);
+      }
+    }
   }
 }
 
