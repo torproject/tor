b2646b8bfd0f31a97c90e3b73598c5f47c3093f3
Roger Dingledine <arma@torproject.org>
2004-09-28 22:38:21 +0000

    fix some local buffer overflows fix a memory leak
    svn:r2391

--- trunk/src/or/dirserv.c
+++ trunk/src/or/dirserv.c
@@ -677,7 +677,7 @@ void dirserv_set_cached_directory(const char *directory, time_t when)
     }
     cached_directory_published = when;
     if(get_data_directory(&options)) {
-      sprintf(filename,"%s/cached-directory", get_data_directory(&options));
+      snprintf(filename,sizeof(filename),"%s/cached-directory", get_data_directory(&options));
       if(write_str_to_file(filename,cached_directory,0) < 0) {
         log_fn(LOG_WARN, "Couldn't write cached directory to disk. Ignoring.");
       }
@@ -751,7 +751,7 @@ static int dirserv_regenerate_directory(void)
   }
   free(new_directory);
   if(get_data_directory(&options)) {
-    sprintf(filename,"%s/cached-directory", get_data_directory(&options));
+    snprintf(filename,sizeof(filename),"%s/cached-directory", get_data_directory(&options));
     if(write_str_to_file(filename,the_directory,0) < 0) {
       log_fn(LOG_WARN, "Couldn't write cached directory to disk. Ignoring.");
     }
