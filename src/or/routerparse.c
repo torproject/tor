b08ff7b1f72ce89a013c17cd7915c3af7c9e476e
Nick Mathewson <nickm@torproject.org>
2004-10-06 13:28:34 +0000

    Fix a couple of typos, one of which conceals a bug in parsing opt keywords with objects
    svn:r2421

--- src/or/routerparse.c
+++ src/or/routerparse.c
@@ -1147,7 +1147,7 @@ get_next_token(const char **s, where_syntax where) {
       tok->args[0] = tor_strndup(*s,next-*s);
       tok->n_args = 1;
       *s = eat_whitespace_no_nl(next+1);
-      a_syn = OBJ_OK;
+      o_syn = OBJ_OK;
     } else {
       tok->tp = _UNRECOGNIZED;
       next = strchr(*s, '\n');
@@ -1213,7 +1213,7 @@ get_next_token(const char **s, where_syntax where) {
       break;
     case NEED_KEY:
       if (!tok->key)
-        RET_ERR("Missing publid key for keyword");
+        RET_ERR("Missing public key for keyword");
       break;
     case OBJ_OK:
       break;
