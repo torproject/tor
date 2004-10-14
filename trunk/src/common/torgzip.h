a5054626ec745818a184729a19f055999bb07b88
Nick Mathewson <nickm@torproject.org>
2004-10-14 04:31:16 +0000

    fix tab/space in torgzip.h
    svn:r2493

--- trunk/src/common/torgzip.h
+++ trunk/src/common/torgzip.h
@@ -14,12 +14,12 @@ typedef enum { GZIP_METHOD=1, ZLIB_METHOD=2 } compress_method_t;
 
 int
 tor_gzip_compress(char **out, size_t *out_len,
-		  const char *in, size_t in_len,
-		  compress_method_t method);
+                  const char *in, size_t in_len,
+                  compress_method_t method);
 int
 tor_gzip_uncompress(char **out, size_t *out_len,
-		    const char *in, size_t in_len,
-		    compress_method_t method);
+                    const char *in, size_t in_len,
+                    compress_method_t method);
 
 int is_gzip_supported(void);
 
