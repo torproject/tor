cad26b343dba0ee1047a0053256e2b9845f90743
Roger Dingledine <arma@torproject.org>
2004-11-09 07:20:21 +0000

    once normalized, DataDirectory is always defined
    svn:r2734

--- src/or/dirserv.c
+++ src/or/dirserv.c
@@ -711,11 +711,9 @@ void dirserv_set_cached_directory(const char *directory, time_t when)
       log_fn(LOG_WARN,"Error compressing cached directory");
     }
     cached_directory_published = when;
-    if(get_options()->DataDirectory) {
-      tor_snprintf(filename,sizeof(filename),"%s/cached-directory", get_options()->DataDirectory);
-      if(write_str_to_file(filename,cached_directory,0) < 0) {
-        log_fn(LOG_WARN, "Couldn't write cached directory to disk. Ignoring.");
-      }
+    tor_snprintf(filename,sizeof(filename),"%s/cached-directory", get_options()->DataDirectory);
+    if(write_str_to_file(filename,cached_directory,0) < 0) {
+      log_fn(LOG_WARN, "Couldn't write cached directory to disk. Ignoring.");
     }
   }
 }
