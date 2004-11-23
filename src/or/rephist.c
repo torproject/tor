921c40abe71707ca04eb99b63cf0234580b3d551
Roger Dingledine <arma@torproject.org>
2004-11-23 09:01:37 +0000

    fail more gracefully next time
    svn:r2954

--- src/or/rephist.c
+++ src/or/rephist.c
@@ -588,6 +588,7 @@ rep_hist_get_bandwidth_lines(void)
   cp = buf;
   for (r=0;r<2;++r) {
     b = r?read_array:write_array;
+    tor_assert(b);
     format_iso_time(t, b->next_period-NUM_SECS_BW_SUM_INTERVAL);
     tor_snprintf(cp, len-(cp-buf), "opt %s %s (%d s) ", r?"read-history ":"write-history", t,
             NUM_SECS_BW_SUM_INTERVAL);
