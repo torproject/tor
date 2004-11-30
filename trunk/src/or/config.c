0fdf60bff54989dbc9e691668cb64c13bf449c5e
Roger Dingledine <arma@torproject.org>
2004-11-30 08:44:19 +0000

    write down our "how to have two defaults for dirfetchperiod" issue, so we don't forget about it.
    svn:r3040

--- trunk/src/or/config.c
+++ trunk/src/or/config.c
@@ -105,7 +105,9 @@ static config_var_t config_vars[] = {
   VAR("DataDirectory",       STRING,   DataDirectory,        NULL),
   VAR("DirPort",             UINT,     DirPort,              "0"),
   VAR("DirBindAddress",      LINELIST, DirBindAddress,       NULL),
-  VAR("DirFetchPeriod",      INTERVAL, DirFetchPeriod,       "1 hours"),
+/* XXX we'd like dirfetchperiod to be higher for people with dirport not
+ * set, but low for people with dirport set. how to have two defaults? */
+  VAR("DirFetchPeriod",      INTERVAL, DirFetchPeriod,       "1 hour"),
   VAR("DirPostPeriod",       INTERVAL, DirPostPeriod,        "20 minutes"),
   VAR("RendPostPeriod",      INTERVAL, RendPostPeriod,       "20 minutes"),
   VAR("DirPolicy",           LINELIST, DirPolicy,            NULL),
