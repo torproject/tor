50a314c93190aa200ac3c788e48c08e098233c17
Roger Dingledine <arma@torproject.org>
2004-12-05 11:58:30 +0000

    int is not necessarily the same size as size_t
    svn:r3079

--- src/common/torint.h
+++ src/common/torint.h
@@ -233,7 +233,7 @@ typedef uint32_t uintptr_t;
 #endif
 
 /* Any size_t larger than this amount is likely to be an underflow. */
-#define SIZE_T_CEILING (1u<<(sizeof(size_t)*8 - 1))
+#define SIZE_T_CEILING (sizeof(char)<<(sizeof(size_t)*8 - 1))
 
 #endif /* __TORINT_H */
 
