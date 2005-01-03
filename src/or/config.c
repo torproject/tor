d7a0f1a62936b4a6c51abe953632ab9acbc49f8e
Roger Dingledine <arma@torproject.org>
2005-01-03 23:21:32 +0000

    update print_usage to use new url
    svn:r3267

--- src/or/config.c
+++ src/or/config.c
@@ -871,7 +871,7 @@ print_usage(void)
   printf(
 "Copyright 2001-2004 Roger Dingledine, Nick Mathewson, Matej Pfajfar.\n\n"
 "tor -f <torrc> [args]\n"
-"See man page for options, or http://freehaven.net/tor/ for documentation.\n");
+"See man page for options, or http://tor.eff.org/ for documentation.\n");
 }
 
 /**
