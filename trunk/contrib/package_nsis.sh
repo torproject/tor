937ce4cf79de6128514d23276ec47fd1bc4ca3df
Nick Mathewson <nickm@torproject.org>
2005-03-23 21:28:37 +0000

    Remove @LOCALSTATEDIR@ macros when building windows package
    svn:r3842

--- trunk/contrib/package_nsis.sh
+++ trunk/contrib/package_nsis.sh
@@ -24,7 +24,11 @@ man2html doc/tor.1.in > win_tmp/tmp/tor-reference.html
 man2html doc/tor-resolve.1 > win_tmp/tmp/tor-resolve.html
 
 clean_newlines() {
-    perl -pe 'BEGIN {undef $;} s/^\n$/\r\n/mg; s/([^\r])\n$/\1\r\n/mg;' $1 >$2
+    perl -pe 'BEGIN {undef $/;} s/^\n$/\r\n/mg; s/([^\r])\n$/\1\r\n/mg;' $1 >$2
+}
+
+clean_localstatedir() {
+    perl -pe 'BEGIN {undef $/;} s/^\n$/\r\n/mg; s/([^\r])\n$/\1\r\n/mg; s{\@LOCALSTATEDIR\@/(lib|log)/tor/}{\\Application Data\\Tor\\}' $1 >$2
 }
 
 for fn in CLIENTS tor-spec.txt HACKING rend-spec.txt control-spec.txt \
@@ -42,7 +46,7 @@ for fn in README AUTHORS ChangeLog LICENSE; do \
     clean_newlines $fn win_tmp/$fn
 done
 
-clean_newlines src/config/torrc.sample.in win_tmp/src/config/torrc.sample
+clean_localstatedir src/config/torrc.sample.in win_tmp/src/config/torrc.sample
 
 cp contrib/tor.nsi win_tmp/contrib
 
