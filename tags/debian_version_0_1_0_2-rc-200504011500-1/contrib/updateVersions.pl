609ac57f4d1402a7400af20e8a62bb08a0e8575b
(no author) <(no author)@55e972cd-5a19-0410-ae62-a4d7a52db4cd>
2005-04-01 16:33:54 +0000

    This commit was manufactured by cvs2svn to create tag 'debian_version_0_1_0_2-rc-200504011500-1'.
    svn:r3973

---
+++ tags/debian_version_0_1_0_2-rc-200504011500-1/contrib/updateVersions.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl -w
+
+$CONFIGURE_IN = './configure.in';
+$ORCONFIG_H = './src/win32/orconfig.h';
+$TOR_NSI = './contrib/tor.nsi';
+
+sub demand {
+    my $fn = shift;
+    die "Missing file $fn" unless (-f $fn);
+}
+
+demand($CONFIGURE_IN);
+demand($ORCONFIG_H);
+demand($TOR_NSI);
+
+# extract version from configure.in
+
+open(F, $CONFIGURE_IN) or die "$!";
+$version = undef;
+while (<F>) {
+    if (/AM_INIT_AUTOMAKE\(tor,\s*([^\)]*)\)/) {
+	$version = $1;
+	last;
+    }
+}
+die "No version found" unless $version;
+print "Tor version is $version\n";
+close F;
+
+sub correctversion {
+    my ($fn, $defchar) = @_;
+    undef $/;
+    open(F, $fn) or die "$!";
+    my $s = <F>;
+    close F;
+    if ($s =~ /^$defchar(?:)define\s+VERSION\s+\"([^\"]+)\"/m) {
+	$oldver = $1;
+	if ($oldver ne $version) {
+	    print "Version mismatch in $fn: It thinks that the version is $oldver.  Fixing.\n";
+	    $line = $defchar . "define VERSION \"$version\"";
+	    open(F, ">$fn.bak");
+	    print F $s;
+	    close F;
+	    $s =~ s/^$defchar(?:)define\s+VERSION.*?$/$line/m;
+	    open(F, ">$fn");
+	    print F $s;
+	    close F;	    
+	} else {
+	    print "$fn has the correct version. Good.\n";
+	}
+    } else {
+	print "Didn't find a version line in $fn -- uh oh.\n";
+    }
+}
+
+correctversion($TOR_NSI, "!");
+correctversion($ORCONFIG_H, "#");
