fd18ac1591f6e036848a6d8f9f07903a1c04ed9b
Roger Dingledine <arma@torproject.org>
2005-04-01 09:28:14 +0000

    when you decide you're reachable, actually publish your descriptor right then.
    svn:r3968

--- trunk/src/or/router.c
+++ trunk/src/or/router.c
@@ -416,6 +416,7 @@ void router_orport_found_reachable(void) {
     if (!clique_mode(get_options()))
       log_fn(LOG_NOTICE,"Your ORPort is reachable from the outside. Excellent. Publishing server descriptor.");
     can_reach_or_port = 1;
+    consider_publishable_server(time(NULL), 1);
   }
 }
 
