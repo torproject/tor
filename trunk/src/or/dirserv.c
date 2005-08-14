0bc1cd0a73ca0e48b6d723af4bda5a32c12c4ebd
Roger Dingledine <arma@torproject.org>
2005-08-14 08:38:34 +0000

    when listing min-bandwidth servers in the controller, don't get fooled by a low bandwidthrate
    svn:r4777

--- trunk/src/or/dirserv.c
+++ trunk/src/or/dirserv.c
@@ -463,7 +463,9 @@ dirserver_getinfo_unregistered(const char *question)
   for (i = 0; i < smartlist_len(descriptor_list); ++i) {
     ent = smartlist_get(descriptor_list, i);
     r = dirserv_router_fingerprint_is_known(ent);
-    if (ent->bandwidthcapacity >= min_bw && r == 0) {
+    if (ent->bandwidthcapacity >= min_bw &&
+        ent->bandwidthrate >= min_bw &&
+        r == 0) {
       /* then log this one */
       tor_snprintf(buf, sizeof(buf),
                    "%s: BW %d on '%s'.",
