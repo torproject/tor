0c54c7689e6fe0e8a8922c23b58b93767a117ee0
Roger Dingledine <arma@torproject.org>
2006-04-11 14:12:04 +0000

    ok, put moria1 back in, since it's v1 and thus we need to send our rendezvous descriptors to it.
    eventually we might make a 'v1only' tag that explains it's only
    for rendezvous descriptors.
    
    
    svn:r6382

--- trunk/src/or/config.c
+++ trunk/src/or/config.c
@@ -526,6 +526,9 @@ add_default_trusted_dirservers(void)
 {
   int i;
   const char *dirservers[] = {
+    /* eventually we should mark moria1 as "v1only" */
+    "moria1 v1 18.244.0.188:9031 "
+      "FFCB 46DB 1339 DA84 674C 70D7 CB58 6434 C437 0441",
     "moria2 v1 18.244.0.114:80 "
       "719B E45D E224 B607 C537 07D0 E214 3E2D 423E 74CF",
     "tor26 v1 86.59.21.38:80 "
