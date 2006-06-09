8b6e0c0fdf9ac48ae245220dba0f777a9e0169ca
Roger Dingledine <arma@torproject.org>
2006-06-09 02:45:39 +0000

    fix a bootstrapping check we ignored that prevents us from running with only one dir authority.
    svn:r6570

--- trunk/src/or/routerlist.c
+++ trunk/src/or/routerlist.c
@@ -3442,10 +3442,8 @@ update_router_descriptor_client_downloads(time_t now)
     return;
   }
 
-  /* XXX here's another magic 2 that probably should be replaced
-   * by <= smartlist_len(trusted_dir_servers)/2
-   * or by a function returning same.  -- weasel */
-  if (networkstatus_list && smartlist_len(networkstatus_list) < 2) {
+  if (networkstatus_list && smartlist_len(networkstatus_list) <=
+                            smartlist_len(trusted_dir_servers)/2) {
     log_info(LD_DIR,
              "Not enough networkstatus documents to launch requests.");
   }
