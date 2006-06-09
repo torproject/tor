1db2c714db2ad367d0529ef2ceee319f4e632b5e
Nick Mathewson <nickm@torproject.org>
2006-06-09 17:07:42 +0000

    Backport escape_for_log() fixes.
    svn:r6580

--- src/common/util.c
+++ src/common/util.c
@@ -609,7 +609,7 @@ esc_for_log(const char *s)
         len += 2;
         break;
       default:
-        if (TOR_ISPRINT(*cp))
+        if (TOR_ISPRINT(*cp) && ((uint8_t)*cp)<127)
           ++len;
         else
           len += 4;
@@ -640,10 +640,10 @@ esc_for_log(const char *s)
         *outp++ = 'r';
         break;
       default:
-        if (TOR_ISPRINT(*cp)) {
+        if (TOR_ISPRINT(*cp) && ((uint8_t)*cp)<127) {
           *outp++ = *cp;
         } else {
-          tor_snprintf(outp, 5, "\\%03o", (uint8_t) *cp);
+          tor_snprintf(outp, 5, "\\%03o", (int)(uint8_t) *cp);
           outp += 4;
         }
         break;
