c063c7b8cb8574dfc6f84f0268fad28c52bc84fe
Nick Mathewson <nickm@torproject.org>
2006-09-07 19:00:51 +0000

    r8757@Kushana:  nickm | 2006-09-07 13:07:46 -0400  Fix more compile warnings on Woody.
    svn:r8349

--- src/or/eventdns_tor.h
+++ src/or/eventdns_tor.h
@@ -1,8 +1,12 @@
 
 #include "orconfig.h"
 #define DNS_USE_OPENSSL_FOR_ID
+#ifndef HAVE_UINT
 typedef unsigned int uint;
+#endif
+#ifndef HAVE_U_CHAR
 typedef unsigned char u_char;
+#endif
 #ifdef MS_WINDOWS
 #define inline __inline
 #endif
