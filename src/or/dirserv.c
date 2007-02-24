dde4bc1fd3a97c664410b98d5918e1a05e4278dc
Nick Mathewson <nickm@torproject.org>
2007-02-24 23:55:36 +0000

    r11933@catbus:  nickm | 2007-02-24 18:55:33 -0500  Fix a bug in 9572: after we decref a defunct v1 directory, drop the main reference to it.  Should fix bug 396.  Many thanks to seeess for tracking this one down.
    svn:r9648

--- src/or/dirserv.c
+++ src/or/dirserv.c
@@ -1153,6 +1153,7 @@ dirserv_clear_old_v1_info(time_t now)
   if (cached_directory &&
       cached_directory->published < (now - MAX_V1_DIRECTORY_AGE)) {
     cached_dir_decref(cached_directory);
+    cached_directory = NULL;
   }
   if (cached_runningrouters.published < (now - MAX_V1_RR_AGE)) {
     clear_cached_dir(&cached_runningrouters);
