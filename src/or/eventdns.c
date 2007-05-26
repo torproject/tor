b0be93c8b0f933ca246bfae8d384bfd99efac33d
Nick Mathewson <nickm@torproject.org>
2007-05-26 00:15:58 +0000

    r12988@catbus:  nickm | 2007-05-25 20:15:56 -0400  socklen_t is a silly type anyway. Just use int as the argument for evdns_server_request_get_requesting_addr.
    svn:r10347

--- src/or/eventdns.c
+++ src/or/eventdns.c
@@ -1812,10 +1812,10 @@ evdns_server_request_drop(struct evdns_server_request *_req)
 
 // exported function
 int
-evdns_server_request_get_requesting_addr(struct evdns_server_request *_req, struct sockaddr *sa, socklen_t addr_len)
+evdns_server_request_get_requesting_addr(struct evdns_server_request *_req, struct sockaddr *sa, int addr_len)
 {
 	struct server_request *req = TO_SERVER_REQUEST(_req);
-	if (addr_len < req->addrlen)
+	if (addr_len < (int)req->addrlen)
 		return -1;
 	memcpy(sa, &(req->addr), req->addrlen);
 	return req->addrlen;
