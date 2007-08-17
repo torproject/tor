c5893e50358dda27158835374769f4e394bedc2d
Nick Mathewson <nickm@torproject.org>
2007-08-17 21:55:24 +0000

    r14641@catbus:  nickm | 2007-08-17 17:53:14 -0400  Stop running ./configure from autogen.sh.  "Let's see if anybody complains!" says Roger.
    svn:r11159

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Run this to generate all the initial makefiles, etc.
-aclocal
-autoheader
-autoconf
-automake --add-missing --copy
+aclocal && \
+	autoheader && \
+	autoconf && \
+	automake --add-missing --copy
