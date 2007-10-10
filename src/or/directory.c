b599d4264faccf631899d586a5386697d12cedc8
Nick Mathewson <nickm@torproject.org>
2007-10-10 19:33:06 +0000

    r15633@catbus:  nickm | 2007-10-10 14:16:54 -0400  Fix a spelling mistake.
    svn:r11838

--- src/or/directory.c
+++ src/or/directory.c
@@ -1436,7 +1436,7 @@ connection_dir_client_reached_eof(dir_connection_t *conn)
       tor_free(body); tor_free(headers); tor_free(reason);
       return -1;
     }
-    log_info(LD_DIR,"Received authority certificatess (size %d) from server "
+    log_info(LD_DIR,"Received authority certificates (size %d) from server "
              "'%s:%d'",(int) body_len, conn->_base.address, conn->_base.port);
     if (trusted_dirs_load_certs_from_string(body, 0)<0) {
       log_warn(LD_DIR, "Unable to parse fetched certificates");
