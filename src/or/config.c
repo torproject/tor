8ca51869fdcd653406dd908d1661a25315393bf9
Roger Dingledine <arma@torproject.org>
2007-11-11 01:06:16 +0000

    Allow people to say PreferTunnelledDirConns rather than PreferTunneledDirConns, for those alternate-spellers out there.
    svn:r12466

--- src/or/config.c
+++ src/or/config.c
@@ -81,6 +81,7 @@ static config_abbrev_t _option_abbrevs[] = {
   { "NumEntryNodes", "NumEntryGuards", 0, 0},
   { "ResolvConf", "ServerDNSResolvConfFile", 0, 1},
   { "SearchDomains", "ServerDNSSearchDomains", 0, 1},
+  { "PreferTunnelledDirConns", "PreferTunneledDirConns", 0, 0},
   { NULL, NULL, 0, 0},
 };
 /* A list of state-file abbreviations, for compatibility. */
