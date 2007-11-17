d6f8ba25c92430cbc0ba9f14da4c7518e309eb0e
Roger Dingledine <arma@torproject.org>
2007-11-17 01:53:33 +0000

    back out r12509 and fix a better bug instead: When authorities detected more than two relays running on the same IP address, they were clearing all the status flags but forgetting to clear the "hsdir" flag. So clients were being told that a given relay was the right choice for a v2 hsdir lookup, yet they never had its descriptor because it was marked as 'not running' in the consensus.
    svn:r12515

--- src/or/dirserv.c
+++ src/or/dirserv.c
@@ -2022,7 +2022,10 @@ dirserv_generate_networkstatus_vote_obj(crypto_pk_env_t *private_key,
       if (digestmap_get(omit_as_sybil, ri->cache_info.identity_digest)) {
         rs->is_authority = rs->is_exit = rs->is_stable = rs->is_fast =
           rs->is_running = rs->is_named = rs->is_valid = rs->is_v2_dir =
-          rs->is_possible_guard = 0;
+          rs->is_hs_dir = rs->is_possible_guard = 0;
+        /* FFFF we might want some mechanism to check later on if we
+         * missed zeroing any flags: it's easy to add a new flag but
+         * forget to add it to this clause. */
       }
       if (!vote_on_reachability)
         rs->is_running = 0;
