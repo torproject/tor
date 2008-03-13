ac6cc430473f6a43c2b26cf88131a0339c67b66a
Nick Mathewson <nickm@torproject.org>
2008-03-13 15:06:26 +0000

    r18781@catbus:  nickm | 2008-03-13 11:00:51 -0400  Have OpenBSD_malloc_Linux.c use SIZE_T_MAX from torint.h, instead of checking cpu macros.  There is always one more cpu you havent checked for.
    svn:r14000

--- src/common/OpenBSD_malloc_Linux.c
+++ src/common/OpenBSD_malloc_Linux.c
@@ -54,6 +54,8 @@
 #include <limits.h>
 #include <errno.h>
 #include <err.h>
+/* For SIZE_T_MAX */
+#include "torint.h"
 
 //#include "thread_private.h"
 
@@ -1926,11 +1928,15 @@ realloc(void *ptr, size_t size)
 	return (r);
 }
 
+#if 0
 #if defined(__i386__)||defined(__arm__)||defined(__powerpc__)
 #define SIZE_MAX 0xffffffff
 #endif
 #if defined(__x86_64__)
 #define SIZE_MAX 0xffffffffffffffff
+#endif
+#else
+#define SIZE_MAX SIZE_T_MAX
 #endif
 
 void *
