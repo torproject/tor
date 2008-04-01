39c402c29f750b25d041419cb945c7bd35db6eeb
Roger Dingledine <arma@torproject.org>
2008-04-01 21:05:31 +0000

    when add_file_log() fails, tell us why.
    svn:r14277

--- src/or/config.c
+++ src/or/config.c
@@ -3902,7 +3902,8 @@ options_init_logs(or_options_t *options, int validate_only)
         !strcasecmp(smartlist_get(elts,0), "file")) {
       if (!validate_only) {
         if (add_file_log(severity, smartlist_get(elts, 1)) < 0) {
-          log_warn(LD_CONFIG, "Couldn't open file for 'Log %s'", opt->value);
+          log_warn(LD_CONFIG, "Couldn't open file for 'Log %s': %s",
+                   opt->value, strerror(errno));
           ok = 0;
         }
       }
