d5bb361977cf1fa8e9a02cf8bd83e94cccfd0add
Roger Dingledine <arma@torproject.org>
2008-08-04 23:59:38 +0000

    minor fixes
    svn:r16407

--- src/or/dirserv.c
+++ src/or/dirserv.c
@@ -1485,7 +1485,7 @@ dirserv_get_directory(void)
   return dirserv_pick_cached_dir_obj(cached_directory, the_directory,
                                      the_directory_is_dirty,
                                      dirserv_regenerate_directory,
-                                     "server directory", V1_AUTHORITY);
+                                     "v1 server directory", V1_AUTHORITY);
 }
 
 /** Only called by v1 auth dirservers.
