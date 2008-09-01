278a89d75a8b9d2b69373bc7e59dfdda996965cc
Nick Mathewson <nickm@torproject.org>
2008-09-01 22:00:07 +0000

    Backport r15821 to 0.2.0: Disallow session resumption during renegotiation.
    svn:r16725

--- src/common/tortls.c
+++ src/common/tortls.c
@@ -564,6 +564,11 @@ tor_tls_context_new(crypto_pk_env_t *identity, unsigned int key_lifetime)
   SSL_CTX_set_options(result->ctx, SSL_OP_NO_SSLv2);
 #endif
   SSL_CTX_set_options(result->ctx, SSL_OP_SINGLE_DH_USE);
+
+#ifdef SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION
+  SSL_CTX_set_options(result->ctx,
+                      SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION);
+#endif
   if (cert && !SSL_CTX_use_certificate(result->ctx,cert))
     goto error;
   X509_free(cert); /* We just added a reference to cert. */
