39117bb422e1f13c689dae62036c815a7171019c
Nick Mathewson <nickm@torproject.org>
2009-01-04 18:52:32 +0000

    Backport r14830: Do not use SSL compression. It was pointless, since most of our cells are encrypted, full of compressed data, or both; and harmful, because compression is expensive and distinguishable.
    svn:r17882

--- src/common/tortls.c
+++ src/common/tortls.c
@@ -564,6 +564,9 @@ tor_tls_context_new(crypto_pk_env_t *identity, unsigned int key_lifetime)
   SSL_CTX_set_options(result->ctx, SSL_OP_NO_SSLv2);
 #endif
   SSL_CTX_set_options(result->ctx, SSL_OP_SINGLE_DH_USE);
+  /* Don't actually allow compression; it uses RAM and time, but the data
+   * we transmit is all encrypted anyway. */
+  result->ctx->comp_methods = NULL;
 
 #ifdef SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION
   SSL_CTX_set_options(result->ctx,
