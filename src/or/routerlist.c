c123163043e42668c3597298e14d25607a2d4623
Roger Dingledine <arma@torproject.org>
2009-01-07 02:13:42 +0000

    Discard router descriptors as we load them if they are more than five days old. Otherwise if Tor is off for a long time and then starts with cached descriptors, it will try to use the onion keys in those obsolete descriptors when building circuits. Bugfix on 0.2.0.x. Fixes bug 887.
    svn:r17993

--- src/or/routerlist.c
+++ src/or/routerlist.c
@@ -3060,6 +3060,12 @@ router_add_to_routerlist(routerinfo_t *router, const char **msg,
     }
   }
 
+  if (!in_consensus && from_cache &&
+      router->cache_info.published_on < time(NULL) - OLD_ROUTER_DESC_MAX_AGE) {
+    *msg = "Router descriptor was really old.";
+    return ROUTER_WAS_NOT_NEW;
+  }
+
   /* We haven't seen a router with this identity before. Add it to the end of
    * the list. */
   routerlist_insert(routerlist, router);
