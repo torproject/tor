fc091e8a96f2e2db9516f4046bec63f1e17a8f71
Nick Mathewson <nickm@torproject.org>
2009-05-12 15:35:53 -0400

    When our bridge status changes, forget old geoip data.
    This fixes bug 932, where all of our usage totals for users when we
    were a regular server would get included in the extrainfo document.

--- src/or/config.c
+++ src/or/config.c
@@ -1329,6 +1329,11 @@ options_act(or_options_t *old_options)
       circuit_expire_all_dirty_circs();
     }
 
+    if (! bool_eq(options->BridgeRelay, old_options->BridgeRelay)) {
+      log_info(LD_GENERAL, "Bridge status changed.  Forgetting GeoIP stats.");
+      geoip_remove_old_clients(time(NULL)+3600);
+    }
+
     if (options_transition_affects_workers(old_options, options)) {
       log_info(LD_GENERAL,
                "Worker-related options changed. Rotating workers.");
