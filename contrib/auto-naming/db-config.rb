2218fd22c580407230995518d9f52b3cd888774f
Roger Dingledine <arma@torproject.org>
2009-09-19 06:22:50 -0400

    replace contrib/auto-naming with a readme saying where it went

--- contrib/auto-naming/db-config.rb
+++
@@ -1,8 +0,0 @@
-$CONFIG             = {} unless $CONFIG
-$CONFIG['database'] = {} unless $CONFIG['database']
-
-# if you use postgres' "ident sameuser" auth set dbhost to ''
-$CONFIG['database']['dbhost'] = 'localhost';
-$CONFIG['database']['dbname'] = 'tornaming';
-$CONFIG['database']['user'] = 'tornaming';
-$CONFIG['database']['password'] = 'x';
