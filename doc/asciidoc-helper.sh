4db5e7ae7698393552c34d2c28180bbefa87fd62
Sebastian Hahn <sebastian@torproject.org>
2010-03-01 03:17:48 +0100

    Add configure switch to disable use of asciidoc
    Also break the build if that switch isn't used and asciidoc isn't
    available.

--- doc/asciidoc-helper.sh
+++ doc/asciidoc-helper.sh
@@ -17,41 +17,21 @@ output=$3
 if [ "$1" = "html" ]; then
     input=${output%%.html.in}.1.txt
     base=${output%%.html.in}
-    if [ "$2" != none ]; then
-      "$2" -d manpage -o $output $input;
-    else
-      echo "==================================";
-      echo;
-      echo "The manpage in html form for $base will ";
-      echo "NOT be available, because asciidoc doesn't appear to be ";
-      echo "installed!";
-      echo;
-      echo "==================================";
-    fi
+    "$2" -d manpage -o $output $input;
 elif [ "$1" = "man" ]; then
     input=${output%%.1.in}.1.txt
     base=${output%%.1.in}
     
-    if test "$2" != none; then
-      if $2 -f manpage $input; then
-        mv $base.1 $output;
-      else
-        echo "==================================";
-        echo;
-        echo "a2x is installed, but some required docbook support files are";
-        echo "missing. Please install docbook-xsl and docbook-xml (Debian)";
-        echo "or similar.";
-        echo;
-        echo "==================================";
-      fi;
+    if "$2" -f manpage $input; then
+      mv $base.1 $output;
     else
       echo "==================================";
       echo;
-      echo "The manpage for $base will NOT be ";
-      echo "available, because a2x doesn't appear to be installed!";
+      echo "a2x is installed, but some required docbook support files are";
+      echo "missing. Please install docbook-xsl and docbook-xml (Debian)";
+      echo "or similar.";
       echo;
       echo "==================================";
+      exit 1;
     fi
 fi
-
-touch $output; \
