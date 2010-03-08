fca673bcd32e184a520edec2b93bc369c29ffe7d
Sebastian Hahn <sebastian@torproject.org>
2010-03-08 21:01:52 +0100

    Don't require asciidoc when building Tor from a tarball
    If asciidoc is required, the user receives an error message telling
    them about the --disable-asciidoc configure switch and the build
    breaks.

--- doc/asciidoc-helper.sh
+++ doc/asciidoc-helper.sh
@@ -17,11 +17,32 @@ output=$3
 if [ "$1" = "html" ]; then
     input=${output%%.html.in}.1.txt
     base=${output%%.html.in}
-    "$2" -d manpage -o $output $input;
+    if [ "$2" != none ]; then
+      "$2" -d manpage -o $output $input;
+    else
+      echo "==================================";
+      echo;
+      echo "You need asciidoc installed to be able to build the manpage.";
+      echo "To build without manpages, use the --disable-asciidoc argument";
+      echo "when calling configure.";
+      echo;
+      echo "==================================";
+      exit 1;
+    fi
 elif [ "$1" = "man" ]; then
     input=${output%%.1.in}.1.txt
     base=${output%%.1.in}
     
+    if test "$2" = none; then
+      echo "==================================";
+      echo;
+      echo "You need asciidoc installed to be able to build the manpage.";
+      echo "To build without manpages, use the --disable-asciidoc argument";
+      echo "when calling configure.";
+      echo;
+      echo "==================================";
+      exit 1;
+    fi
     if "$2" -f manpage $input; then
       mv $base.1 $output;
     else
@@ -35,3 +56,4 @@ elif [ "$1" = "man" ]; then
       exit 1;
     fi
 fi
+
