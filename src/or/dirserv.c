2849a95691c002108666eb414ff497bf93349e7d
Nick Mathewson <nickm@torproject.org>
2010-10-20 13:49:38 -0400

    Add a ! to directory_caches_dir_info() to fix a logic error
    We want to fetch directory info more aggressively if we need it to
    refuseunknownexits.  Thus, we'll want it if our exit policy is _NOT_
    reject *.

--- src/or/dirserv.c
+++ src/or/dirserv.c
@@ -1213,7 +1213,7 @@ directory_caches_dir_info(or_options_t *options)
     return 0;
   /* We need an up-to-date view of network info if we're going to try to
    * block exit attempts from unknown relays. */
-  return router_my_exit_policy_is_reject_star() &&
+  return ! router_my_exit_policy_is_reject_star() &&
     should_refuse_unknown_exits(options);
 }
 
