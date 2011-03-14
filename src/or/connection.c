06f026de6624ea0591b8d08da9c5c69ffa44568c
Nick Mathewson <nickm@torproject.org>
2011-03-14 18:52:15 -0400

    Check newconn, not conn, before marking newconn for close
    This fixes bug 2757.  It's a bugfix on 85da676108f0d.

--- src/or/connection.c
+++ src/or/connection.c
@@ -1178,7 +1178,7 @@ connection_handle_listener_read(connection_t *conn, int new_type)
   }
 
   if (connection_init_accepted_conn(newconn, conn->type) < 0) {
-    if (! conn->marked_for_close)
+    if (! newconn->marked_for_close)
       connection_mark_for_close(newconn);
     return 0;
   }
