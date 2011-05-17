2e07925a5285fd50626e853c48ab5b79eec8e933
Nick Mathewson <nickm@torproject.org>
2011-05-17 19:45:05 -0400

    Oops; that function got renamed.

--- src/or/routerparse.c
+++ src/or/routerparse.c
@@ -4336,7 +4336,7 @@ microdescs_parse_from_string(const char *s, const char *eos,
     }
 
     tok = find_by_keyword(tokens, K_ONION_KEY);
-    if (!crypto_pk_check_key_public_exponent(tok->key)) {
+    if (!crypto_pk_public_exponent_ok(tok->key)) {
       log_warn(LD_DIR,
                "Relay's onion key had invalid exponent.");
       goto next;
