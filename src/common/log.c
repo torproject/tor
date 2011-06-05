9eab601f10958ad75b3aef0015f32fc339609054
George Kadianakis <desnacked@gmail.com>
2011-06-05 21:27:53 +0200

    Add the heartbeat domain in log.c:domain_list[] so that parse_log_domain() doesn't fail.

--- src/common/log.c
+++ src/common/log.c
@@ -899,7 +899,7 @@ log_level_to_string(int level)
 static const char *domain_list[] = {
   "GENERAL", "CRYPTO", "NET", "CONFIG", "FS", "PROTOCOL", "MM",
   "HTTP", "APP", "CONTROL", "CIRC", "REND", "BUG", "DIR", "DIRSERV",
-  "OR", "EDGE", "ACCT", "HIST", "HANDSHAKE", NULL
+  "OR", "EDGE", "ACCT", "HIST", "HANDSHAKE", "HEARTBEAT", NULL
 };
 
 /** Return a bitmask for the log domain for which <b>domain</b> is the name,
