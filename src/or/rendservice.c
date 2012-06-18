4a8eaad7efd4e24e89f969f79d9bde1874f366d1
Nick Mathewson <nickm@torproject.org>
2012-06-18 13:13:53 -0400

    Clear a couple more fields in rend_service_load_auth_keys

--- src/or/rendservice.c
+++ src/or/rendservice.c
@@ -139,7 +139,9 @@ rend_authorized_client_free(rend_authorized_client_t *client)
     return;
   if (client->client_key)
     crypto_pk_free(client->client_key);
+  tor_strclear(client->client_name);
   tor_free(client->client_name);
+  memset(client->descriptor_cookie, 0, sizeof(client->descriptor_cookie));
   tor_free(client);
 }
 
@@ -868,7 +870,7 @@ rend_service_load_auth_keys(rend_service_t *s, const char *hfname)
   if (open_hfile)
     abort_writing_to_file(open_hfile);
  done:
-  /* XXXX zero these two as well */
+  tor_strclear(client_keys_str);
   tor_free(client_keys_str);
   strmap_free(parsed_clients, rend_authorized_client_strmap_item_free);
 
