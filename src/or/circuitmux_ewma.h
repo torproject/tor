bbb2c31d26f2eb1ad46433bcfd321d960a6c7862
Andrea Shepard <andrea@torproject.org>
2012-10-01 01:54:42 -0700

    Move all EWMA code into circuitmux.c and implement circuitmux_policy_t methods using it

--- src/or/circuitmux_ewma.h
+++ src/or/circuitmux_ewma.h
@@ -12,7 +12,17 @@
 #include "or.h"
 #include "circuitmux.h"
 
-/* TODO */
+/* Everything but circuitmux_ewma.c should see this extern */
+#ifndef _TOR_CIRCUITMUX_EWMA_C
+
+extern circuitmux_policy_t ewma_policy;
+
+#endif /* !(_TOR_CIRCUITMUX_EWMA_C) */
+
+/* Externally visible EWMA functions */
+unsigned int cell_ewma_get_tick(void);
+void cell_ewma_set_scale_factor(const or_options_t *options,
+                                const networkstatus_t *consensus);
 
 #endif /* _TOR_CIRCUITMUX_EWMA_H */
 
