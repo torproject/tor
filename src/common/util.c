0558efbd62e0dc5a954cfdaacdd2f48ef9a4abe0
Nick Mathewson <nickm@torproject.org>
2012-12-18 13:33:15 -0500

    Fix a const warning under msvc

--- src/common/util.c
+++ src/common/util.c
@@ -4919,7 +4919,7 @@ tor_check_port_forwarding(const char *filename,
     status = tor_spawn_background(filename, argv, NULL, &child_handle);
 #endif
 
-    tor_free(argv);
+    tor_free((void*)argv);
 
     if (PROCESS_STATUS_ERROR == status) {
       log_warn(LD_GENERAL, "Failed to start port forwarding helper %s",
