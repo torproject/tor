8db8fda43ffdf0a19eb6febfaadb6b1164973807
Nick Mathewson <nickm@torproject.org>
2014-02-03 13:00:15 -0500

    check-spaces fixes for test_router.c

--- src/test/test_router.c
+++ src/test/test_router.c
@@ -11,22 +11,21 @@
 #include "router.h"
 #include "test.h"
 
-
 /** Tese the case when node_get_by_id() returns NULL, node_describe_by_id
  * should return the base 16 encoding of the id.
  */
 static void
 test_node_describe_by_id_null_node(void *arg)
 {
+  const char ID[] = "\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA"
+                    "\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA";
   (void) arg;
-
-  #define ID "\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA"
 
   /* make sure node_get_by_id returns NULL */
   test_assert(!node_get_by_id(ID));
   test_streq(node_describe_by_id(ID),
               "$AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-done:
+ done:
   return;
 }
 
