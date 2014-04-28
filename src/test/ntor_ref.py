f4be34f70d6f277a0f3f73e62ed358564588e92b
Nick Mathewson <nickm@torproject.org>
2014-04-27 22:54:24 -0400

    Make the python test scripts work on python3
    The python scripts invoked by 'make check' didn't work on python3
    before.  That was a problem on systems where 'python' is python3.
    
    Fixes bug 11608; bugfix on 0.2.5.2-alpha.

--- src/test/ntor_ref.py
+++ src/test/ntor_ref.py
@@ -39,13 +39,14 @@ except ImportError:
 import hashlib
 import hmac
 import subprocess
+import sys
 
 # **********************************************************************
 # Helpers and constants
 
 def HMAC(key,msg):
     "Return the HMAC-SHA256 of 'msg' using the key 'key'."
-    H = hmac.new(key, "", hashlib.sha256)
+    H = hmac.new(key, b"", hashlib.sha256)
     H.update(msg)
     return H.digest()
 
@@ -67,10 +68,10 @@ G_LENGTH = 32
 H_LENGTH = 32
 
 PROTOID = b"ntor-curve25519-sha256-1"
-M_EXPAND = PROTOID + ":key_expand"
-T_MAC    = PROTOID + ":mac"
-T_KEY    = PROTOID + ":key_extract"
-T_VERIFY = PROTOID + ":verify"
+M_EXPAND = PROTOID + b":key_expand"
+T_MAC    = PROTOID + b":mac"
+T_KEY    = PROTOID + b":key_extract"
+T_VERIFY = PROTOID + b":verify"
 
 def H_mac(msg): return H(msg, tweak=T_MAC)
 def H_verify(msg): return H(msg, tweak=T_VERIFY)
@@ -91,7 +92,14 @@ class PrivateKey(curve25519mod.Private):
 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-def kdf_rfc5869(key, salt, info, n):
+if sys.version < '3':
+   def int2byte(i):
+      return chr(i)
+else:
+   def int2byte(i):
+      return bytes([i])
+
+def  kdf_rfc5869(key, salt, info, n):
 
     prk = HMAC(key=salt, msg=key)
 
@@ -99,7 +107,7 @@ def kdf_rfc5869(key, salt, info, n):
     last = b""
     i = 1
     while len(out) < n:
-        m = last + info + chr(i)
+        m = last + info + int2byte(i)
         last = h = HMAC(key=prk, msg=m)
         out += h
         i = i + 1
@@ -208,7 +216,7 @@ def server(seckey_b, my_node_id, message, keyBytes=72):
                   pubkey_Y.serialize() +
                   pubkey_X.serialize() +
                   PROTOID +
-                  "Server")
+                  b"Server")
 
     msg = pubkey_Y.serialize() + H_mac(auth_input)
 
@@ -270,7 +278,7 @@ def client_part2(seckey_x, msg, node_id, pubkey_B, keyBytes=72):
                   pubkey_B.serialize() +
                   pubkey_Y.serialize() +
                   pubkey_X.serialize() + PROTOID +
-                  "Server")
+                  b"Server")
 
     my_auth = H_mac(auth_input)
 
@@ -284,7 +292,7 @@ def client_part2(seckey_x, msg, node_id, pubkey_B, keyBytes=72):
 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-def demo(node_id="iToldYouAboutStairs.", server_key=PrivateKey()):
+def demo(node_id=b"iToldYouAboutStairs.", server_key=PrivateKey()):
     """
        Try to handshake with ourself.
     """
@@ -294,7 +302,7 @@ def demo(node_id="iToldYouAboutStairs.", server_key=PrivateKey()):
     assert len(skeys) == 72
     assert len(ckeys) == 72
     assert skeys == ckeys
-    print "OK"
+    print("OK")
 
 # ======================================================================
 def timing():
@@ -304,7 +312,7 @@ def timing():
     import timeit
     t = timeit.Timer(stmt="ntor_ref.demo(N,SK)",
        setup="import ntor_ref,curve25519;N='ABCD'*5;SK=ntor_ref.PrivateKey()")
-    print t.timeit(number=1000)
+    print(t.timeit(number=1000))
 
 # ======================================================================
 
@@ -315,7 +323,7 @@ def kdf_vectors():
     import binascii
     def kdf_vec(inp):
         k = kdf(inp, T_KEY, M_EXPAND, 100)
-        print repr(inp), "\n\""+ binascii.b2a_hex(k)+ "\""
+        print(repr(inp), "\n\""+ binascii.b2a_hex(k)+ "\"")
     kdf_vec("")
     kdf_vec("Tor")
     kdf_vec("AN ALARMING ITEM TO FIND ON YOUR CREDIT-RATING STATEMENT")
@@ -328,13 +336,13 @@ def test_tor():
        Call the test-ntor-cl command-line program to make sure we can
        interoperate with Tor's ntor program
     """
-    enhex=binascii.b2a_hex
+    enhex=lambda s: binascii.b2a_hex(s)
     dehex=lambda s: binascii.a2b_hex(s.strip())
 
-    PROG = "./src/test/test-ntor-cl"
+    PROG = b"./src/test/test-ntor-cl"
     def tor_client1(node_id, pubkey_B):
         " returns (msg, state) "
-        p = subprocess.Popen([PROG, "client1", enhex(node_id),
+        p = subprocess.Popen([PROG, b"client1", enhex(node_id),
                               enhex(pubkey_B.serialize())],
                              stdout=subprocess.PIPE)
         return map(dehex, p.stdout.readlines())
@@ -352,7 +360,7 @@ def test_tor():
         return map(dehex, p.stdout.readlines())
 
 
-    node_id = "thisisatornodeid$#%^"
+    node_id = b"thisisatornodeid$#%^"
     seckey_b = PrivateKey()
     pubkey_B = seckey_b.get_public()
 
@@ -377,14 +385,13 @@ def test_tor():
     assert c_keys == s_keys
     assert len(c_keys) == 90
 
-    print "OK"
+    print("OK")
 
 # ======================================================================
 
 if __name__ == '__main__':
-    import sys
     if len(sys.argv) < 2:
-        print __doc__
+        print(__doc__)
     elif sys.argv[1] == 'gen_kdf_vectors':
         kdf_vectors()
     elif sys.argv[1] == 'timing':
@@ -395,4 +402,4 @@ if __name__ == '__main__':
         test_tor()
 
     else:
-        print __doc__
+        print(__doc__)
--- src/test/slownacl_curve25519.py
+++ src/test/slownacl_curve25519.py
@@ -8,12 +8,14 @@
 
 __all__ = ['smult_curve25519_base', 'smult_curve25519']
 
+import sys
+
 P = 2 ** 255 - 19
 A = 486662
 
 def expmod(b, e, m):
   if e == 0: return 1
-  t = expmod(b, e / 2, m) ** 2 % m
+  t = expmod(b, e // 2, m) ** 2 % m
   if e & 1: t = (t * b) % m
   return t
 
@@ -23,12 +25,14 @@ def inv(x):
 # Addition and doubling formulas taken from Appendix D of "Curve25519:
 # new Diffie-Hellman speed records".
 
-def add((xn,zn), (xm,zm), (xd,zd)):
+def add(n,m,d):
+  (xn,zn), (xm,zm), (xd,zd) = n, m, d
   x = 4 * (xm * xn - zm * zn) ** 2 * zd
   z = 4 * (xm * zn - zm * xn) ** 2 * xd
   return (x % P, z % P)
 
-def double((xn,zn)):
+def double(n):
+  (xn,zn) = n
   x = (xn ** 2 - zn ** 2) ** 2
   z = 4 * xn * zn * (xn ** 2 + A * xn * zn + zn ** 2)
   return (x % P, z % P)
@@ -40,19 +44,34 @@ def curve25519(n, base):
   # (m+1)th multiple of base.
   def f(m):
     if m == 1: return (one, two)
-    (pm, pm1) = f(m / 2)
+    (pm, pm1) = f(m // 2)
     if (m & 1):
       return (add(pm, pm1, one), double(pm1))
     return (double(pm), add(pm, pm1, one))
   ((x,z), _) = f(n)
   return (x * inv(z)) % P
 
+if sys.version < '3':
+    def b2i(c):
+        return ord(c)
+    def i2b(i):
+        return chr(i)
+    def ba2bs(ba):
+        return "".join(ba)
+else:
+    def b2i(c):
+        return c
+    def i2b(i):
+        return i
+    def ba2bs(ba):
+        return bytes(ba)
+
 def unpack(s):
   if len(s) != 32: raise ValueError('Invalid Curve25519 argument')
-  return sum(ord(s[i]) << (8 * i) for i in range(32))
+  return sum(b2i(s[i]) << (8 * i) for i in range(32))
 
 def pack(n):
-  return ''.join([chr((n >> (8 * i)) & 255) for i in range(32)])
+  return ba2bs([i2b((n >> (8 * i)) & 255) for i in range(32)])
 
 def clamp(n):
   n &= ~7
