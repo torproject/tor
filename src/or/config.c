6c512d2f634c5688ca6ce5d4bfbe1ee2d263f212
George Kadianakis <desnacked@riseup.net>
2014-09-15 15:28:27 +0300

    Fix a tor2web log message that referenced the wrong configure switch.

--- src/or/config.c
+++ src/or/config.c
@@ -1378,7 +1378,7 @@ options_act(const or_options_t *old_options)
     log_err(LD_CONFIG, "This copy of Tor was not compiled to run in "
             "'tor2web mode'. It cannot be run with the Tor2webMode torrc "
             "option enabled. To enable Tor2webMode recompile with the "
-            "--enable-tor2webmode option.");
+            "--enable-tor2web-mode option.");
     return -1;
   }
 #endif
