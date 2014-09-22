09183dc3150000007ccc7b8fecd3b5b762a5b698
Roger Dingledine <arma@torproject.org>
2014-09-21 20:02:12 -0400

    clients use optimistic data when reaching hidden services
    Allow clients to use optimistic data when connecting to a hidden service,
    which should cut out the initial round-trip for client-side programs
    including Tor Browser.
    
    (Now that Tor 0.2.2.x is obsolete, all hidden services should support
    server-side optimistic data.)
    
    See proposal 181 for details. Implements ticket 13211.

--- src/or/connection_edge.c
+++ src/or/connection_edge.c
@@ -1767,7 +1767,8 @@ connection_ap_supports_optimistic_data(const entry_connection_t *conn)
      general circuit. */
   if (edge_conn->on_circuit == NULL ||
       edge_conn->on_circuit->state != CIRCUIT_STATE_OPEN ||
-      edge_conn->on_circuit->purpose != CIRCUIT_PURPOSE_C_GENERAL)
+      (edge_conn->on_circuit->purpose != CIRCUIT_PURPOSE_C_GENERAL &&
+       edge_conn->on_circuit->purpose != CIRCUIT_PURPOSE_C_REND_JOINED))
     return 0;
 
   return conn->may_use_optimistic_data;
