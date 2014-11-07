5b1971c7f337371efbab17b88070a7e6f3a11cc8
Nick Mathewson <nickm@torproject.org>
2014-11-07 09:28:49 -0500

    test_checkdir.c: Perhaps this is what will make this test pass on windows?

--- src/test/test_checkdir.c
+++ src/test/test_checkdir.c
@@ -56,7 +56,7 @@ test_checkdir_perms(void *testdata)
   testdir = get_datadir_fname("checkdir_new_groupok_err");
   tt_int_op(0, ==, mkdir(testdir, 027));
   cpd_chkopts = CPD_CHECK_MODE_ONLY|CPD_CREATE|CPD_GROUP_OK;
-  tt_int_op(-1, ==, check_private_dir(testdir, cpd_chkopts, NULL));
+  tt_int_op_nowin(-1, ==, check_private_dir(testdir, cpd_chkopts, NULL));
   tor_free(testdir);
 
   /* test: create new dir, CPD_GROUP_READ option set. */
