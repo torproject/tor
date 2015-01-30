097286e47665a32e54249f809c23e190be9d57e8
Nick Mathewson <nickm@torproject.org>
2015-01-30 14:47:56 -0500

    Fix some unused-argument warnings

--- src/or/rendservice.c
+++ src/or/rendservice.c
@@ -3503,12 +3503,16 @@ set_unix_port(edge_connection_t *conn, rend_service_port_config_t *p)
 static int
 set_unix_port(edge_connection_t *conn, rend_service_port_config_t *p)
 {
+  (void) conn;
+  (void) p;
   return -ENOSYS;
 }
 
 static int
 add_unix_port(smartlist_t *ports, rend_service_port_config_t *p)
 {
+  (void) ports;
+  (void) p;
   return -ENOSYS;
 }
 
