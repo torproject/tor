359faf5e4b4a88663201c2b42dd89a6f77569856
teor <teor2345@gmail.com>
2015-05-06 23:40:23 +1000

    New TestingDirAuthVote{Exit,Guard,HSDir}IsStrict flags
    "option to prevent guard,exit,hsdir flag assignment"
    
    "A node will never receive the corresponding flag unless
    that node is specified in the
    TestingDirAuthVote{Exit,Guard,HSDir} list, regardless of
    its uptime, bandwidth, exit policy, or DirPort".
    
    Patch modified by "teor": VoteOnHidServDirectoriesV2
    is now obsolete, so TestingDirAuthVoteHSDir always
    votes on HSDirs.
    
    Closes ticket 14882. Patch by "robgjansen".
    Commit message and changes file by "teor"
    with quotes from "robgjansen".

--- src/or/dirserv.c
+++ src/or/dirserv.c
@@ -2195,16 +2195,22 @@ set_routerstatus_from_routerinfo(routerstatus_t *rs,
     if (routerset_contains_routerstatus(options->TestingDirAuthVoteExit,
                                         rs, 0)) {
       rs->is_exit = 1;
+    } else if (options->TestingDirAuthVoteExitIsStrict) {
+      rs->is_exit = 0;
     }
 
     if (routerset_contains_routerstatus(options->TestingDirAuthVoteGuard,
                                         rs, 0)) {
       rs->is_possible_guard = 1;
+    } else if (options->TestingDirAuthVoteGuardIsStrict) {
+      rs->is_possible_guard = 0;
     }
 
     if (routerset_contains_routerstatus(options->TestingDirAuthVoteHSDir,
                                         rs, 0)) {
       rs->is_hs_dir = 1;
+    } else if (options->TestingDirAuthVoteHSDirIsStrict) {
+      rs->is_hs_dir = 0;
     }
   }
 }
