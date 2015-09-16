98da122ab4a1f20822a953dd318f995b1ed055ba
Sebastian Hahn <sebastian@torproject.org>
2015-09-16 04:27:32 +0200

    Don't enable SSE2 on X86-64.
    This removes a comment presumably introduced for debugging that was left
    in accidentally. Bug not in any released version of Tor. Fixes bug
    17092.

--- src/ext/ed25519/donna/ed25519-donna-portable.h
+++ src/ext/ed25519/donna/ed25519-donna-portable.h
@@ -148,7 +148,7 @@ static inline void U64TO8_LE(unsigned char *p, const uint64_t v) {
  * architecture supports it.  This is not done on x86-64 as the non-SSE2
  * code benchmarks better, at least on Haswell.
  */
-#if defined(__SSE2__) /* && !defined(CPU_X86_64) */
+#if defined(__SSE2__) && !defined(CPU_X86_64)
 	/* undef in case it's manually specified... */
 	#undef ED25519_SSE2
 	#define ED25519_SSE2
