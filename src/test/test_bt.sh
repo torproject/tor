eb2188168e2eca96692ec5f60f298f7bc287e990
Nick Mathewson <nickm@torproject.org>
2015-09-24 15:07:39 -0400

    Stop trying to generate test scripts via autoconf substitution.
    Use environment variables instead. This repairs 'make distcheck',
    which was running into trouble when it tried to chmod the generated
    scripts.
    
    Fixes 17148.

---
+++ src/test/test_bt.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Test backtrace functionality.
+
+exitcode=0
+
+${builddir:-.}/src/test/test-bt-cl assert | ${PYTHON:-python} "${abs_top_srcdir:-.}/src/test/bt_test.py" || exitcode=1
+${builddir:-.}/src/test/test-bt-cl crash  | ${PYTHON:-python} "${abs_top_srcdir:-.}/src/test/bt_test.py" || exitcode=1
+
+exit ${exitcode}
