85b65bf29f2245eb1363640cddf82e57ba5e7957
Nick Mathewson <nickm@torproject.org>
2015-09-24 15:25:53 -0400

    Add more quotes in the test scripts

--- src/test/test_bt.sh
+++ src/test/test_bt.sh
@@ -3,7 +3,7 @@
 
 exitcode=0
 
-${builddir:-.}/src/test/test-bt-cl assert | ${PYTHON:-python} "${abs_top_srcdir:-.}/src/test/bt_test.py" || exitcode=1
-${builddir:-.}/src/test/test-bt-cl crash  | ${PYTHON:-python} "${abs_top_srcdir:-.}/src/test/bt_test.py" || exitcode=1
+"${builddir:-.}/src/test/test-bt-cl" assert | "${PYTHON:-python}" "${abs_top_srcdir:-.}/src/test/bt_test.py" || exitcode=1
+"${builddir:-.}/src/test/test-bt-cl" crash  | "${PYTHON:-python}" "${abs_top_srcdir:-.}/src/test/bt_test.py" || exitcode=1
 
 exit ${exitcode}
--- src/test/test_ntor.sh
+++ src/test/test_ntor.sh
@@ -3,7 +3,7 @@
 
 exitcode=0
 
-${PYTHON:-python} "${abs_top_srcdir:-.}/src/test/ntor_ref.py" test-tor || exitcode=1
-${PYTHON:-python} "${abs_top_srcdir:-.}/src/test/ntor_ref.py" self-test || exitcode=1
+"${PYTHON:-python}" "${abs_top_srcdir:-.}/src/test/ntor_ref.py" test-tor || exitcode=1
+"${PYTHON:-python}" "${abs_top_srcdir:-.}/src/test/ntor_ref.py" self-test || exitcode=1
 
 exit ${exitcode}
