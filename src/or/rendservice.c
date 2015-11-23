273b267fa234c5a26c9beaacede42a496c880e05
David Goulet <dgoulet@ev0ke.net>
2015-11-23 09:02:54 -0500

    Fix: use the right list in find_expiring_intro_point()
    The wrong list was used when looking up expired intro points in a rend
    service object causing what we think could be reachability issues and
    triggering a BUG log.
    
    Fixes #16702
    
    Signed-off-by: David Goulet <dgoulet@ev0ke.net>

--- src/or/rendservice.c
+++ src/or/rendservice.c
@@ -3038,7 +3038,8 @@ find_expiring_intro_point(rend_service_t *service, origin_circuit_t *circ)
   tor_assert(TO_CIRCUIT(circ)->purpose == CIRCUIT_PURPOSE_S_ESTABLISH_INTRO ||
              TO_CIRCUIT(circ)->purpose == CIRCUIT_PURPOSE_S_INTRO);
 
-  SMARTLIST_FOREACH(service->intro_nodes, rend_intro_point_t *, intro_point,
+  SMARTLIST_FOREACH(service->expiring_nodes, rend_intro_point_t *,
+                    intro_point,
     if (crypto_pk_eq_keys(intro_point->intro_key, circ->intro_key)) {
       return intro_point;
   });
