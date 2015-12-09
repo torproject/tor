fbdd32ebe9021d34dbe314a9cb24480ff3a9c9b1
cypherpunks <cypherpunks@torproject.org>
2015-12-09 13:07:35 +0100

    Mention the expected length of the digests
    Some functions that use digest maps did not mention that the digests are
    expected to have DIGEST_LEN bytes. This lead to buffer over-reads in the
    past.

--- src/or/rendcache.c
+++ src/or/rendcache.c
@@ -321,9 +321,9 @@ rend_cache_failure_purge(void)
 }
 
 /** Lookup the rend failure cache using a relay identity digest in
- * <b>identity</b> and service ID <b>service_id</b>. If found, the intro
- * failure is set in <b>intro_entry</b> else it stays untouched. Return 1
- * iff found else 0. */
+ * <b>identity</b> which has DIGEST_LEN bytes and service ID <b>service_id</b>
+ * which is a null-terminated string. If found, the intro failure is set in
+ * <b>intro_entry</b> else it stays untouched. Return 1 iff found else 0. */
 STATIC int
 cache_failure_intro_lookup(const uint8_t *identity, const char *service_id,
                            rend_cache_failure_intro_t **intro_entry)
