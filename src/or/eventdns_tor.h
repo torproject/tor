62c4d3880fa0e9929c575a66a7c4f464498a7c5e
cypherpunks <cypherpunks@torproject.org>
2015-12-10 16:47:39 +0100

    Remove eventdns specific inline definition
    The header includes compat.h which already defines inline.

--- src/or/eventdns_tor.h
+++ src/or/eventdns_tor.h
@@ -12,9 +12,6 @@ typedef unsigned int uint;
 #ifndef HAVE_U_CHAR
 typedef unsigned char u_char;
 #endif
-#ifdef _WIN32
-#define inline __inline
-#endif
 #include "torint.h"
 
 /* These are for debugging possible memory leaks. */
