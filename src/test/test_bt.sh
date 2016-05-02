b72aa18d73869563e88b85930408781495deed2c
Nick Mathewson <nickm@torproject.org>
2016-05-02 12:58:58 -0400

    test_bt.sh: Check stderr for backtrace as well as stdout.
    addresssanitizer likes to put backtraces on stderr.

--- src/test/test_bt.sh
+++ src/test/test_bt.sh
@@ -4,7 +4,7 @@
 exitcode=0
 
 "${builddir:-.}/src/test/test-bt-cl" backtraces || exit $?
-"${builddir:-.}/src/test/test-bt-cl" assert | "${PYTHON:-python}" "${abs_top_srcdir:-.}/src/test/bt_test.py" || exitcode="$?"
-"${builddir:-.}/src/test/test-bt-cl" crash  | "${PYTHON:-python}" "${abs_top_srcdir:-.}/src/test/bt_test.py" || exitcode="$?"
+"${builddir:-.}/src/test/test-bt-cl" assert 2>&1 | "${PYTHON:-python}" "${abs_top_srcdir:-.}/src/test/bt_test.py" || exitcode="$?"
+"${builddir:-.}/src/test/test-bt-cl" crash  2>&1 | "${PYTHON:-python}" "${abs_top_srcdir:-.}/src/test/bt_test.py" || exitcode="$?"
 
 exit ${exitcode}
