19054ab18aef794d7d1e2054d10fd73a87fc6c94
Sebastian Hahn <sebastian@torproject.org>
2016-07-03 04:32:54 +0200

    Remove HAVE_EVENT_* defines from winconfig
    They were accidentally left sitting around in 517e0f965.

--- src/win32/orconfig.h
+++ src/win32/orconfig.h
@@ -17,15 +17,6 @@
 /* Define to 1 if you have the <errno.h> header file. */
 #define HAVE_ERRNO_H
 
-/* Define to 1 if you have the `event_get_method' function. */
-#define HAVE_EVENT_GET_METHOD 1
-
-/* Define to 1 if you have the `event_get_version' function. */
-#define HAVE_EVENT_GET_VERSION 1
-
-/* Define to 1 if you have the `event_set_log_callback' function. */
-#define HAVE_EVENT_SET_LOG_CALLBACK 1
-
 /* Define to 1 if you have the <fcntl.h> header file. */
 #define HAVE_FCNTL_H
 
@@ -240,7 +231,6 @@
 #define HAVE_EVENT2_EVENT_H
 #define SHARE_DATADIR ""
 #define HAVE_EVENT2_DNS_H
-#define HAVE_EVENT_BASE_LOOPEXIT
 #define USE_CURVE25519_DONNA
 
 #define ENUM_VALS_ARE_SIGNED 1
