0ba3444b4a9d408573a442b32be794a78b9250b5
David Goulet <dgoulet@torproject.org>
2016-09-01 10:07:41 -0400

    Make trunnel find files recursively
    Signed-off-by: David Goulet <dgoulet@torproject.org>

--- scripts/codegen/run_trunnel.sh
+++ scripts/codegen/run_trunnel.sh
@@ -5,7 +5,11 @@ if test "x$TRUNNEL_PATH" != "x"; then
   export PYTHONPATH
 fi
 
-python -m trunnel --require-version=1.4 ./src/trunnel/*.trunnel
+# Get all .trunnel files recursively from that directory so we can support
+# multiple sub-directories.
+for file in `find ./src/trunnel/ -name '*.trunnel'`; do
+  python -m trunnel --require-version=1.4 $file
+done
 
 python -m trunnel --require-version=1.4 --write-c-files --target-dir=./src/ext/trunnel/
 
