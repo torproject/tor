782b6ec288a553f66e47157d377ec4b200b54394
Roger Dingledine <arma@torproject.org>
2016-10-02 01:12:27 -0400

    Bridges and relays now use microdescriptors
    (like clients do) rather than old-style router descriptors. Now bridges
    will blend in with clients in terms of the circuits they build.
    
    Fixes bug 6769; bugfix on 0.2.3.2-alpha.

--- src/or/microdesc.c
+++ src/or/microdesc.c
@@ -919,12 +919,8 @@ we_use_microdescriptors_for_circuits(const or_options_t *options)
 {
   int ret = options->UseMicrodescriptors;
   if (ret == -1) {
-    /* UseMicrodescriptors is "auto"; we need to decide: */
-    /* We'll use microdescriptors iff we are
-     * not a server, and we're not autofetching everything. */
-    /* XXXX++ what does not being a server have to do with it? also there's
-     * a partitioning issue here where bridges differ from clients. */
-    ret = !server_mode(options) && !options->FetchUselessDescriptors;
+    /* UseMicrodescriptors is "auto"; choose yes. */
+    return 1;
   }
   return ret;
 }
