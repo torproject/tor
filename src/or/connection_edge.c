14a604721091d3995c9905efe52e9f2fb789afaa
Roger Dingledine <arma@torproject.org>
2016-10-02 19:34:23 -0400

    remove whitespace accidentally added in 4f1a04ff

--- src/or/connection_edge.c
+++ src/or/connection_edge.c
@@ -1,4 +1,4 @@
- /* Copyright (c) 2001 Matej Pfajfar.
+/* Copyright (c) 2001 Matej Pfajfar.
  * Copyright (c) 2001-2004, Roger Dingledine.
  * Copyright (c) 2004-2006, Roger Dingledine, Nick Mathewson.
  * Copyright (c) 2007-2016, The Tor Project, Inc. */
