a5130de43297373ec93c3623689e42cc1384b3f9
Nick Mathewson <nickm@torproject.org>
2017-03-26 12:30:00 +0200

    Tweak test_entrynodes comment

--- src/test/test_entrynodes.c
+++ src/test/test_entrynodes.c
@@ -584,7 +584,7 @@ test_entry_guard_parse_from_state_full(void *arg)
   smartlist_t *text = smartlist_new();
   char *joined = NULL;
 
-  // So nodes aren't expired
+  // So nodes aren't expired. This is Tue, 13 Dec 2016 09:37:14 GMT
   update_approx_time(1481621834);
 
   MOCK(entry_guard_is_listed, mock_entry_guard_is_listed);
