236cf49670d1a4995e0494675ee4fbf155e6f90a
Alexander Færøy <ahf@torproject.org>
2017-04-25 17:51:58 +0200

    Add stub function for libor-trace.
    OS X's ar(1) doesn't allow us to create an archive with no object files.
    This patch adds a stub file with a stub function in it to make OS X
    happy again.

---
+++ src/trace/trace.c
@@ -0,0 +1,11 @@
+/* Copyright (c) 2017, The Tor Project, Inc. */
+/* See LICENSE for licensing information */
+
+#include "trace.h"
+
+/** Initialize the tracing library. */
+void
+tor_trace_init(void)
+{
+}
+
---
+++ src/trace/trace.h
@@ -0,0 +1,10 @@
+/* Copyright (c) 2017, The Tor Project, Inc. */
+/* See LICENSE for licensing information */
+
+#ifndef TOR_TRACE_TRACE_H
+#define TOR_TRACE_TRACE_H
+
+void tor_trace_init(void);
+
+#endif // TOR_TRACE_TRACE_H
+
