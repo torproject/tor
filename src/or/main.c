2549b3e9233372556ae41c51b2e89c3ddb21a9ea
David Goulet <dgoulet@torproject.org>
2017-08-25 09:28:10 -0400

    sandbox: Fix double free when initializing HSv3 filenames
    Don't free a reference that has been stolen.
    
    Fixes #23329
    
    Signed-off-by: David Goulet <dgoulet@torproject.org>

--- src/or/main.c
+++ src/or/main.c
@@ -3583,7 +3583,6 @@ sandbox_init_filter(void)
       /* steals references */
       sandbox_cfg_allow_open_filename(&cfg, file_name);
       sandbox_cfg_allow_open_filename(&cfg, tmp_name);
-      tor_free(file_name);
     });
     SMARTLIST_FOREACH(dirs, char *, dir, {
       /* steals reference */
