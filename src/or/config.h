48ee85e0c6c83c31693bc58ca4e8e91c8031421d
Nick Mathewson <nickm@torproject.org>
2017-10-23 12:20:58 -0400

    Have LOG_PROTOCOL_WARN call its own function
    Also, make the function that implements LOG_PROTOCOL_WARN use a
    cached value of the desired loglevel, rather than calling
    get_options().

--- src/or/config.h
+++ src/or/config.h
@@ -31,6 +31,7 @@ const char *safe_str_client(const char *address);
 const char *safe_str(const char *address);
 const char *escaped_safe_str_client(const char *address);
 const char *escaped_safe_str(const char *address);
+int get_protocol_warning_severity_level(void);
 const char *get_version(void);
 const char *get_short_version(void);
 setopt_err_t options_trial_assign(config_line_t *list, unsigned flags,
