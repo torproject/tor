bb7c5b431d12808c8cfda0cf9029bc1c0b2d12c6
teor <teor2345@gmail.com>
2017-12-08 12:49:33 +1100

    Move a comment to relay_send_end_cell_from_edge()
    It looks like it was left behind in a refactor.
    
    Fixes 24559.

--- src/or/connection_edge.c
+++ src/or/connection_edge.c
@@ -339,6 +339,10 @@ relay_send_end_cell_from_edge(streamid_t stream_id, circuit_t *circ,
 
   payload[0] = (char) reason;
 
+  /* Note: we have to use relay_send_command_from_edge here, not
+   * connection_edge_end or connection_edge_send_command, since those require
+   * that we have a stream connected to a circuit, and we don't connect to a
+   * circuit until we have a pending/successful resolve. */
   return relay_send_command_from_edge(stream_id, circ, RELAY_COMMAND_END,
                                       payload, 1, cpath_layer);
 }
@@ -3404,11 +3408,6 @@ connection_exit_begin_conn(cell_t *cell, circuit_t *circ)
   relay_header_unpack(&rh, cell->payload);
   if (rh.length > RELAY_PAYLOAD_SIZE)
     return -END_CIRC_REASON_TORPROTOCOL;
-
-  /* Note: we have to use relay_send_command_from_edge here, not
-   * connection_edge_end or connection_edge_send_command, since those require
-   * that we have a stream connected to a circuit, and we don't connect to a
-   * circuit until we have a pending/successful resolve. */
 
   if (!server_mode(options) &&
       circ->purpose != CIRCUIT_PURPOSE_S_REND_JOINED) {
