60bf8f6cf6295808eec542be31649b0bd82ad926
Nick Mathewson <nickm@torproject.org>
2017-12-20 09:12:38 -0500

    Improve add_msec test to cover ms->sec rollover
    Per mikeperry's suggestion, I'm making sure that the millisecond
    part of the time will always roll over in this test.

--- src/test/test_util.c
+++ src/test/test_util.c
@@ -5970,6 +5970,16 @@ test_util_monotonic_time_add_msec(void *arg)
   tt_i64_op(monotime_diff_msec(&t1, &t2), OP_EQ, 1337);
   tt_i64_op(monotime_coarse_diff_msec(&ct1, &ct2), OP_EQ, 1337);
 
+  /* Add 1337 msec twice more; make sure that any second rollover issues
+   * worked. */
+  monotime_add_msec(&t2, &t2, 1337);
+  monotime_coarse_add_msec(&ct2, &ct2, 1337);
+  monotime_add_msec(&t2, &t2, 1337);
+  monotime_coarse_add_msec(&ct2, &ct2, 1337);
+  tt_i64_op(monotime_diff_msec(&t1, &t2), OP_EQ, 1337*3);
+  tt_i64_op(monotime_coarse_diff_msec(&ct1, &ct2), OP_EQ, 1337*3);
+
+  
  done:
   ;
 }
