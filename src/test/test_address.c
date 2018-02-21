fc22bcadb51bcabb9b9c16a9768adbe86788b7f1
Nick Mathewson <nickm@torproject.org>
2018-02-21 09:35:48 -0500

    Revert 4438ef32's changes to test_address.c
    Apparently some versions of the mac sdk care about the ordering of
    net/if.h wrt other headers.
    
    Fixes bug 25319; bug not in any released tor.

--- src/test/test_address.c
+++ src/test/test_address.c
@@ -11,11 +11,8 @@
 #include <iphlpapi.h>
 #endif
 
-#ifdef HAVE_NET_IF_H
-#include <net/if.h>
-#endif
-
 #ifdef HAVE_IFADDRS_TO_SMARTLIST
+#include <net/if.h>
 #include <ifaddrs.h>
 #endif
 
@@ -23,6 +20,7 @@
 #ifdef HAVE_SYS_IOCTL_H
 #include <sys/ioctl.h>
 #endif
+#include <net/if.h>
 #endif /* defined(HAVE_IFCONF_TO_SMARTLIST) */
 
 #include "or.h"
