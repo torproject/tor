77e73e62405eb4e79cdd41d5e9c80a437ce10475
Isis Lovecruft <isis@torproject.org>
2018-03-21 00:24:46 +0000

    rust: Implement error types for Rust protover implementation.
    This will allow us to do actual error handling intra-crate in a more
    rusty manner, e.g. propogating errors in match statements, conversion
    between error types, logging messages, etc.
    
     * FIXES part of #24031: https://bugs.torproject.org/24031.

--- src/rust/protover/lib.rs
+++ src/rust/protover/lib.rs
@@ -22,6 +22,8 @@
 //! protocols to develop independently, without having to claim compatibility
 //! with specific versions of Tor.
 
+#[deny(missing_docs)]
+
 extern crate libc;
 extern crate smartlist;
 extern crate external;
@@ -32,6 +34,7 @@ extern crate tor_util;
 #[macro_use]
 extern crate tor_log;
 
+pub mod errors;
 mod protover;
 pub mod ffi;
 
