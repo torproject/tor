5e6ffec17c52ac6483bf112fddadd5132c71ee0a
Nick Mathewson <nickm@torproject.org>
2018-04-12 16:32:22 -0400

    fixup! Add a new token-bucket backend abstraction, with tests
    Fix a copy-paste error in an earlier fixup commit

--- src/common/token_bucket.c
+++ src/common/token_bucket.c
@@ -68,7 +68,7 @@ token_bucket_adjust(token_bucket_t *bucket,
   bucket->rate = rate_per_sec_to_rate_per_step(rate);
   bucket->burst = burst;
   bucket->read_bucket = MIN(bucket->read_bucket, (int32_t)burst);
-  bucket->read_bucket = MIN(bucket->write_bucket, (int32_t)burst);
+  bucket->write_bucket = MIN(bucket->write_bucket, (int32_t)burst);
 }
 
 /**
