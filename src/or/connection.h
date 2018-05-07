03e4465cfa8bdc4b65951e1b74bb95226d41745a
rl1987 <rl1987@sdf.lonestar.org>
2018-05-07 14:26:27 +0200

    Minor code cleanups

--- src/or/connection.h
+++ src/or/connection.h
@@ -15,6 +15,17 @@
 /* XXXX For buf_datalen in inline function */
 #include "buffers.h"
 
+/**
+ * This struct associates an old listener connection to be replaced
+ * by new connection described by port configuration. Only used when
+ * moving listeners to/from wildcard IP address.
+ */
+typedef struct
+{
+  connection_t *old_conn; /* Old listener connection to be replaced */
+  const port_cfg_t *new_port; /* New port configuration */
+} listener_replacement_t;
+
 const char *conn_type_to_string(int type);
 const char *conn_state_to_string(int type, int state);
 int conn_listener_type_supports_af_unix(int type);
