a0933a7c38e8764a45b857289b00ec214037ce1c
rl1987 <rl1987@sdf.lonestar.org>
2018-05-16 17:51:10 +0200

    Use bool_neq instead of XOR

--- src/or/connection.c
+++ src/or/connection.c
@@ -2716,9 +2716,9 @@ retry_listener_ports(smartlist_t *old_conns,
           break;
         }
 #ifdef ENABLE_LISTENER_REBIND
-        int may_need_rebind = port_matches_exact &
-                              (tor_addr_is_null(&wanted->addr) ^
-                               tor_addr_is_null(&conn->addr));
+        int may_need_rebind =
+          port_matches_exact & bool_neq(tor_addr_is_null(&wanted->addr),
+                                        tor_addr_is_null(&conn->addr));
         if (replacements && may_need_rebind) {
           listener_replacement_t *replacement =
             tor_malloc(sizeof(listener_replacement_t));
