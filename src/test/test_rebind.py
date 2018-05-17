9b2905f2ea9632bcc5108b4deb4a71659c45cca8
rl1987 <rl1987@sdf.lonestar.org>
2018-05-17 21:55:40 +0200

    squash! Integration test for socket rebinding

--- src/test/test_rebind.py
+++ src/test/test_rebind.py
@@ -12,6 +12,12 @@ def try_connecting_to_socksport():
         sys.exit('Cannot connect to SOCKSPort')
     socks_socket.close()
 
+def wait_for_log(s):
+    while True:
+        l = tor_process.stdout.readline()
+        if s in l:
+            return
+
 if not os.path.exists(sys.argv[1]):
     sys.exit('ERROR: cannot find tor at %s' % sys.argv[1])
 
@@ -20,7 +26,9 @@ tor_path = sys.argv[1]
 tor_process = subprocess.Popen([tor_path,
                                '-ControlPort', '127.0.0.1:9053', 
                                '-SOCKSPort', '127.0.0.1:9052',
-                               '-FetchServerDescriptors', '0'])
+                               '-FetchServerDescriptors', '0'],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
 
 if tor_process == None:
     sys.exit('ERROR: running tor failed')
@@ -28,7 +36,7 @@ if tor_process == None:
 if len(sys.argv) < 2:
      sys.exit('Usage: %s <path-to-tor>' % sys.argv[0])
 
-time.sleep(1) # TODO: Wait for 'Opening Control listener on'
+wait_for_log('Opening Control listener on')
 
 try_connecting_to_socksport()
 
@@ -40,12 +48,12 @@ if control_socket.connect_ex(('127.0.0.1', 9053)):
 
 control_socket.sendall('AUTHENTICATE \r\n')
 control_socket.sendall('SETCONF SOCKSPort=0.0.0.0:9052\r\n')
-time.sleep(0.1)
+wait_for_log('Closing no-longer-configured Socks listener')
 
 try_connecting_to_socksport()
 
 control_socket.sendall('SETCONF SOCKSPort=127.0.0.1:9052\r\n')
-time.sleep(0.1)
+wait_for_log('Closing no-longer-configured Socks listener')
 
 try_connecting_to_socksport()
 
