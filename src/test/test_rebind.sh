b04fed91383ba8c25a58dd91b84e43deb1318909
rl1987 <rl1987@sdf.lonestar.org>
2018-05-20 19:38:57 +0200

    fixup! Integration test for socket rebinding

--- src/test/test_rebind.sh
+++ src/test/test_rebind.sh
@@ -2,6 +2,6 @@
 
 exitcode=0
 
-"${PYTHON:-python}" "${abs_top_srcdir:-.}/src/test/test_rebind.py" ${abs_top_srcdir:-.}/src/or/tor || exitcode=1
+"${PYTHON:-python}" "${abs_top_srcdir:-.}/src/test/test_rebind.py" "${TESTING_TOR_BINARY}" || exitcode=1
 
 exit ${exitcode}
