eb7a3fae08638e38e55014b293d347d500cf14fa
rl1987 <rl1987@sdf.lonestar.org>
2018-05-30 19:44:20 +0200

    Refrain from mentioning old bug in a warning

--- src/or/rendcache.c
+++ src/or/rendcache.c
@@ -908,9 +908,7 @@ rend_cache_store_v2_desc_as_client(const char *desc,
     if (n_intro_points <= 0) {
       log_warn(LD_REND, "Failed to parse introduction points. Either the "
                "service has published a corrupt descriptor or you have "
-               "provided invalid authorization data, or (maybe!) the "
-               "server is deliberately serving broken data in an attempt "
-               "to crash you with bug 21018.");
+               "provided invalid authorization data.");
       goto err;
     } else if (n_intro_points > MAX_INTRO_POINTS) {
       log_warn(LD_REND, "Found too many introduction points on a hidden "
