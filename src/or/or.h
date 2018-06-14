6896ab28b29f614f774fa39145d36586d14d1f22
Nick Mathewson <nickm@torproject.org>
2018-06-14 16:19:11 -0400

    Move entry_port_cfg_t into its own header.

--- src/or/or.h
+++ src/or/or.h
@@ -84,6 +84,10 @@
 #include "util_format.h"
 #include "hs_circuitmap.h"
 
+// These, more than other includes, are for keeping the other struct
+// definitions working. We should remove them when we minimize our includes.
+#include "entry_port_cfg_st.h"
+
 /* These signals are defined to help handle_control_signal work.
  */
 #ifndef SIGHUP
@@ -1243,46 +1247,7 @@ typedef struct {
 } relay_header_t;
 
 typedef struct socks_request_t socks_request_t;
-
-typedef struct entry_port_cfg_t {
-  /* Client port types (socks, dns, trans, natd) only: */
-  uint8_t isolation_flags; /**< Zero or more isolation flags */
-  int session_group; /**< A session group, or -1 if this port is not in a
-                      * session group. */
-
-  /* Socks only: */
-  /** When both no-auth and user/pass are advertised by a SOCKS client, select
-   * no-auth. */
-  unsigned int socks_prefer_no_auth : 1;
-  /** When ISO_SOCKSAUTH is in use, Keep-Alive circuits indefinitely. */
-  unsigned int socks_iso_keep_alive : 1;
-
-  /* Client port types only: */
-  unsigned int ipv4_traffic : 1;
-  unsigned int ipv6_traffic : 1;
-  unsigned int prefer_ipv6 : 1;
-  unsigned int dns_request : 1;
-  unsigned int onion_traffic : 1;
-
-  /** For a socks listener: should we cache IPv4/IPv6 DNS information that
-   * exit nodes tell us?
-   *
-   * @{ */
-  unsigned int cache_ipv4_answers : 1;
-  unsigned int cache_ipv6_answers : 1;
-  /** @} */
-  /** For a socks listeners: if we find an answer in our client-side DNS cache,
-   * should we use it?
-   *
-   * @{ */
-  unsigned int use_cached_ipv4_answers : 1;
-  unsigned int use_cached_ipv6_answers : 1;
-  /** @} */
-  /** For socks listeners: When we can automap an address to IPv4 or IPv6,
-   * do we prefer IPv6? */
-  unsigned int prefer_ipv6_virtaddr : 1;
-
-} entry_port_cfg_t;
+typedef struct entry_port_cfg_t entry_port_cfg_t;
 
 typedef struct server_port_cfg_t {
   /* Server port types (or, dir) only: */
