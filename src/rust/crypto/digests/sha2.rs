508332feaf99ada12037c3a7fa77eab8516bb69e
Isis Lovecruft <isis@torproject.org>
2018-06-18 18:57:38 +0000

    rust: Add "test-c-from-rust" feature-gate.
    Due to linker issues (#25386) when testing Rust code which calls C,
    all tests which touch FFI code should now be feature-gated behind the
    "test-c-from-rust" flag.  To run this test code, cargo must be called
    with `cargo test --features="test-c-from-rust"`.
    
     * FIXES #26398: https://bugs.torproject.org/26398

--- src/rust/crypto/digests/sha2.rs
+++ src/rust/crypto/digests/sha2.rs
@@ -165,15 +165,19 @@ impl FixedOutput for Sha512 {
 
 #[cfg(test)]
 mod test {
+    #[cfg(feature = "test-c-from-rust")]
     use digest::Digest;
 
+    #[cfg(feature = "test-c-from-rust")]
     use super::*;
 
+    #[cfg(feature = "test-c-from-rust")]
     #[test]
     fn sha256_default() {
         let _: Sha256 = Sha256::default();
     }
 
+    #[cfg(feature = "test-c-from-rust")]
     #[test]
     fn sha256_digest() {
         let mut h: Sha256 = Sha256::new();
@@ -190,11 +194,13 @@ mod test {
         assert_eq!(&result[..], &b"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"[..]);
     }
 
+    #[cfg(feature = "test-c-from-rust")]
     #[test]
     fn sha512_default() {
         let _: Sha512 = Sha512::default();
     }
 
+    #[cfg(feature = "test-c-from-rust")]
     #[test]
     fn sha512_digest() {
         let mut h: Sha512 = Sha512::new();
