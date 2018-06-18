6af248f27f887a9a36a94a9f87a7b7ce66896c08
Nick Mathewson <nickm@torproject.org>
2018-06-18 16:10:47 -0400

    Finally remove our EOL@EOF check.
    This check was added by mistake long ago.  It will be nice to see
    these EOLs disappear from our code over time.

--- scripts/maint/checkSpace.pl
+++ scripts/maint/checkSpace.pl
@@ -126,7 +126,7 @@ for my $fn (@ARGV) {
             ## Warn about double semi-colons at the end of a line.
             if (/;;$/) {
                 msg "       double semi-colons at the end of $. in $fn\n"
-            }            
+            }
             ## Warn about multiple internal spaces.
             #if (/[^\s,:]\s{2,}[^\s\\=]/) {
             #    msg "     X  X:$fn:$.\n";
@@ -191,11 +191,6 @@ for my $fn (@ARGV) {
                 }
             }
         }
-    }
-    ## Warn if the file doesn't end with a blank line.
-    #    (End each file with a single blank line.)
-    if (! $lastnil) {
-        msg "  EOL\@EOF:$fn:$.\n";
     }
     close(F);
 }
