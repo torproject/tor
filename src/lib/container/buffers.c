365179cd16b8ccc0986d7a99db2607a95d0932a9
Nick Mathewson <nickm@torproject.org>
2018-06-28 17:21:15 -0400

    Fix a bogus n in a comment

--- src/lib/container/buffers.c
+++ src/lib/container/buffers.c
@@ -1,6 +1,6 @@
 /* Copyright (c) 2001 Matej Pfajfar.
  * Copyright (c) 2001-2004, Roger Dingledine.
-n * Copyright (c) 2004-2006, Roger Dingledine, Nick Mathewson.
+ * Copyright (c) 2004-2006, Roger Dingledine, Nick Mathewson.
  * Copyright (c) 2007-2018, The Tor Project, Inc. */
 /* See LICENSE for licensing information */
 
