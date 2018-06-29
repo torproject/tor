35e1c497aa54a22818552c9f3757067e381bb6bb
Nick Mathewson <nickm@torproject.org>
2018-06-29 11:15:42 -0400

    Combine compat.h into util.h
    This is now just a collection of frequently-used headers.

--- src/common/compat.h
+++ src/common/compat.h
@@ -6,24 +6,6 @@
 #ifndef TOR_COMPAT_H
 #define TOR_COMPAT_H
 
-#include "orconfig.h"
-#include "lib/cc/torint.h"
-#include "lib/testsupport/testsupport.h"
-#include "lib/cc/compat_compiler.h"
-#include "lib/arch/bytes.h"
-#include "lib/time/compat_time.h"
-#include "lib/string/compat_ctype.h"
-#include "lib/string/compat_string.h"
-#include "lib/string/printf.h"
-#include "lib/net/socket.h"
-#include "lib/net/ipv4.h"
-#include "lib/net/ipv6.h"
-#include "lib/net/resolve.h"
-#include "lib/fs/files.h"
-#include "lib/fs/mmap.h"
-#include "lib/fs/userdb.h"
-#include "lib/wallclock/timeval.h"
-#include "lib/intmath/cmp.h"
-#include "lib/thread/threads.h"
+#include "common/util.h"
 
 #endif /* !defined(TOR_COMPAT_H) */
