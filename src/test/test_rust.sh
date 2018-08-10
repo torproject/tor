229a75a49a440e61c97dca820cf59360470e1560
teor <teor@torproject.org>
2018-08-10 11:54:26 +1000

    Rust: use a consistent working directory in builds and tests
    cd to ${abs_top_builddir}/src/rust before running cargo in
    src/test/test_rust.sh.
    
    Fixes bug 26497; bugfix on 0.3.3.2-alpha.

--- src/test/test_rust.sh
+++ src/test/test_rust.sh
@@ -7,7 +7,7 @@ export LSAN_OPTIONS=suppressions=${abs_top_srcdir:-../../..}/src/test/rust_supp.
 
 for cargo_toml_dir in "${abs_top_srcdir:-../../..}"/src/rust/*; do
     if [ -e "${cargo_toml_dir}/Cargo.toml" ]; then
-	cd "${cargo_toml_dir}" && \
+	cd "${abs_top_builddir:-../../..}/src/rust" && \
 	    CARGO_TARGET_DIR="${abs_top_builddir:-../../..}/src/rust/target" \
 	    "${CARGO:-cargo}" test ${CARGO_ONLINE-"--frozen"} \
 	    ${EXTRA_CARGO_OPTIONS} \
