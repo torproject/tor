e1291aa84a04b8006130ce08338ee07a662e5ce0
teor <teor@torproject.org>
2018-08-10 13:11:27 +1000

    Rust: Use --all-features in test_rust.sh for 0.3.3 and 0.3.4
    Re-applies 0.3.3 changes after 24629.

--- src/test/test_rust.sh
+++ src/test/test_rust.sh
@@ -9,7 +9,7 @@ for cargo_toml_dir in "${abs_top_srcdir:-../../..}"/src/rust/*; do
     if [ -e "${cargo_toml_dir}/Cargo.toml" ]; then
 	cd "${abs_top_builddir:-../../..}/src/rust" && \
 	    CARGO_TARGET_DIR="${abs_top_builddir:-../../..}/src/rust/target" \
-	    "${CARGO:-cargo}" test ${CARGO_ONLINE-"--frozen"} \
+	    "${CARGO:-cargo}" test --all-features ${CARGO_ONLINE-"--frozen"} \
 	    ${EXTRA_CARGO_OPTIONS} \
 	    --manifest-path "${cargo_toml_dir}/Cargo.toml" || exitcode=1
     fi
