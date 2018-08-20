e08e36f80af86482a94ce9deea05b625643ae9a5
Mike Perry <mikeperry-git@torproject.org>
2018-08-20 19:59:01 +0000

    fixup! Ticket #25573: Track half-closed stream ids
    Make half-conn search take a const.

--- src/or/connection_edge.c
+++ src/or/connection_edge.c
@@ -492,7 +492,7 @@ connection_half_edge_add(const edge_connection_t *conn,
 }
 
 static half_edge_t *
-connection_half_edge_find_stream_id(smartlist_t *half_conns,
+connection_half_edge_find_stream_id(const smartlist_t *half_conns,
                                     streamid_t stream_id)
 {
   if (!half_conns)
