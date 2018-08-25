60478b1c9627bf95fc2903c2e8caeda3159a584f
Mike Perry <mikeperry-git@torproject.org>
2018-08-25 02:15:23 +0000

    fixup! Ticket #25573: Sort+bsearch the half-closed connection list
    Bah. Check for empty list in end.

--- src/or/connection_edge.c
+++ src/or/connection_edge.c
@@ -639,6 +639,9 @@ connection_half_edge_is_valid_end(smartlist_t *half_conns,
   half_edge_t *half;
   int found, remove_idx;
 
+  if (!half_conns)
+    return 0;
+
   remove_idx = smartlist_bsearch_idx(half_conns, &stream_id,
                                     connection_half_edge_compare_bsearch,
                                     &found);
