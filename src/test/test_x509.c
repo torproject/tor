eeba944ee0a642ef105b639f1bb0580fae28d3ab
Nick Mathewson <nickm@torproject.org>
2018-09-04 20:25:25 -0400

    Fix an easy refcounting bug in a unit test

--- src/test/test_x509.c
+++ src/test/test_x509.c
@@ -57,7 +57,6 @@ test_x509_cert_new_failing_digest(void *arg)
  done:
   crypto_pk_free(pk1);
   crypto_pk_free(pk2);
-  tor_x509_cert_impl_free(impl);
   UNMOCK(crypto_digest);
   teardown_capture_of_logs();
 }
