d8280216c0c940fef33982a3d98524e61abf7634
Nick Mathewson <nickm@torproject.org>
2018-09-13 17:54:26 -0400

    Include torint.h in socketpair.c for intptr_t definition.

--- src/lib/net/socketpair.c
+++ src/lib/net/socketpair.c
@@ -2,6 +2,7 @@
  * Copyright (c) 2004-2006, Roger Dingledine, Nick Mathewson.
  * Copyright (c) 2007-2018, The Tor Project, Inc. */
 
+#include "lib/cc/torint.h"
 #include "lib/net/socketpair.h"
 #include "lib/net/inaddr_st.h"
 #include "lib/arch/bytes.h"
