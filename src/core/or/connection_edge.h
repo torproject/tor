8d444753b059335ae9e6fb8b8e201c58e8f1a593
Nick Mathewson <nickm@torproject.org>
2018-09-18 15:07:02 -0400

    Use the correct function signatures in test_relaycell.c
    This is now officially an antipattern: please let's never copy a
    function declaration in two places again.  That's what headers are
    for.

--- src/core/or/connection_edge.h
+++ src/core/or/connection_edge.h
@@ -254,6 +254,12 @@ STATIC void connection_ap_handshake_rewrite(entry_connection_t *conn,
                                             rewrite_result_t *out);
 
 STATIC int connection_ap_process_http_connect(entry_connection_t *conn);
+struct half_edge_t;
+STATIC void connection_half_edge_add(const edge_connection_t *conn,
+                                     origin_circuit_t *circ);
+STATIC struct half_edge_t *connection_half_edge_find_stream_id(
+                                     const smartlist_t *half_conns,
+                                     streamid_t stream_id);
 #endif /* defined(CONNECTION_EDGE_PRIVATE) */
 
 #endif /* !defined(TOR_CONNECTION_EDGE_H) */
