7829e3a86875da16e3d7ac55be85145bd672fc12
rl1987 <rl1987@sdf.lonestar.org>
2018-10-12 14:19:40 +0300

    First testcase for get_next_token()

--- src/test/test_parsecommon.c
+++ src/test/test_parsecommon.c
@@ -34,11 +34,38 @@ test_parsecommon_tokenize_string_null(void *arg)
   return;
 }
 
+static void
+test_parsecommon_get_next_token_success(void *arg)
+{
+  memarea_t *area = memarea_new();
+  const char *str = "uptime 1024";
+  const char *end = str + strlen(str);
+  const char **s = &str;
+  token_rule_t table = T01("uptime", K_UPTIME, GE(1), NO_OBJ);
+  (void)arg;
+
+  directory_token_t *token = get_next_token(area, s, end, &table);
+
+  tt_int_op(token->tp, OP_EQ, K_UPTIME);
+  tt_int_op(token->n_args, OP_EQ, 1);
+  tt_str_op(*(token->args), OP_EQ, "1024");
+  tt_assert(!token->object_type);
+  tt_int_op(token->object_size, OP_EQ, 0);
+  tt_assert(!token->object_body);
+
+  tt_ptr_op(*s, OP_EQ, end);
+
+ done:
+  memarea_drop_all(area);
+  return;
+}
+
 #define PARSECOMMON_TEST(name) \
   { #name, test_parsecommon_ ## name, 0, NULL, NULL }
 
 struct testcase_t parsecommon_tests[] = {
   PARSECOMMON_TEST(tokenize_string_null),
+  PARSECOMMON_TEST(get_next_token_success),
   END_OF_TESTCASES
 };
 
