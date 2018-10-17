5cc655da6bba91995520d18b6dc67bfe33540463
George Kadianakis <desnacked@riseup.net>
2018-10-17 14:28:27 +0300

    HSv2: Close introduction circuits when client auth settings change.

--- src/feature/rend/rendservice.c
+++ src/feature/rend/rendservice.c
@@ -67,7 +67,7 @@
 #endif
 
 struct rend_service_t;
-static origin_circuit_t *find_intro_circuit(rend_intro_point_t *intro,
+static origin_circuit_t *find_intro_circuit(const rend_intro_point_t *intro,
                                             const char *pk_digest);
 static rend_intro_point_t *find_intro_point(origin_circuit_t *circ);
 static rend_intro_point_t *find_expiring_intro_point(
@@ -546,11 +546,26 @@ copy_service_on_prunning(rend_service_t *dst, rend_service_t *src)
   dst->n_intro_points_wanted = src->n_intro_points_wanted;
 }
 
+/** Close the introduction circuits of <b>s</b>. This function does not remove
+ *  the intro points from the service. */
+static void
+close_intro_circs_of_service(const rend_service_t *s)
+{
+  SMARTLIST_FOREACH_BEGIN(s->intro_nodes, const rend_intro_point_t *, intro) {
+    origin_circuit_t *intro_circ = find_intro_circuit(intro, s->pk_digest);
+    if (!intro_circ) {
+      continue;
+    }
+    circuit_mark_for_close(TO_CIRCUIT(intro_circ), END_CIRC_REASON_FINISHED);
+  } SMARTLIST_FOREACH_END(intro);
+}
+
 /** A SIGHUP occured and service pruning might have happened; some services
  *  might not be available anymore. Check if any introduction circuits are now
  *  useless and need to be cleaned up. */
 static void
-clean_intro_circs_after_service_prune(const smartlist_t *surviving_services) {
+clean_intro_circs_after_service_prune(const smartlist_t *surviving_services)
+{
   origin_circuit_t *ocirc = NULL;
 
   /* For every service introduction circuit we can find, see if we have a
@@ -649,6 +664,17 @@ rend_service_prune_list_impl_(void)
           strcmp(old->directory, new->directory)) {
         continue;
       }
+
+      /* If client authorization settings changed, kill the introduction
+       * circuits since they should be rebuilt to reflect the new client auth
+       * settings. We don't actually remove the intro points from the
+       * 'intro_nodes' list, since they should remain the same and the service
+       * should just make new circuits. */
+      if (old->auth_type != new->auth_type) {
+        log_info(LD_REND, "Client auth config changed: Closing intro circs.");
+        close_intro_circs_of_service(old);
+      }
+
       smartlist_add_all(new->intro_nodes, old->intro_nodes);
       smartlist_clear(old->intro_nodes);
       smartlist_add_all(new->expiring_nodes, old->expiring_nodes);
@@ -3564,7 +3590,7 @@ rend_service_rendezvous_has_opened(origin_circuit_t *circuit)
  * found.
  */
 static origin_circuit_t *
-find_intro_circuit(rend_intro_point_t *intro, const char *pk_digest)
+find_intro_circuit(const rend_intro_point_t *intro, const char *pk_digest)
 {
   origin_circuit_t *circ = NULL;
 
