75ba1cf57be801d4d19d8495a18fe4e9efb9c8fe
George Kadianakis <desnacked@riseup.net>
2018-12-07 16:35:23 +0200

    Concentrate all TOR_USEC_PER_SEC definitions in a single header file.

---
+++ src/lib/defs/time.h
@@ -0,0 +1,24 @@
+/* Copyright (c) 2001, Matej Pfajfar.
+ * Copyright (c) 2001-2004, Roger Dingledine.
+ * Copyright (c) 2004-2006, Roger Dingledine, Nick Mathewson.
+ * Copyright (c) 2007-2018, The Tor Project, Inc. */
+/* See LICENSE for licensing information */
+
+#ifndef TOR_TIME_DEFS_H
+#define TOR_TIME_DEFS_H
+
+/**
+ * \file time.h
+ *
+ * \brief Definitions for timing-related constants.
+ **/
+
+/** How many usecs per second */
+#define TOR_USEC_PER_SEC (1000000)
+
+/** How many nanoseconds per usec */
+#define TOR_NSEC_PER_USEC (1000)
+/* How many nanoseconds per msec */
+#define TOR_NSEC_PER_MSEC (1000*1000)
+
+#endif
