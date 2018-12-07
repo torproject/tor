ac7995b452eb04dc08238d34f5b47f1ca3f581ea
Nick Mathewson <nickm@torproject.org>
2018-12-07 11:41:39 -0500

    fixup! Call run_tor_main_loop() in ntmain.c, rather than do_main_loop().

--- src/app/main/ntmain.c
+++ src/app/main/ntmain.c
@@ -298,7 +298,7 @@ nt_service_body(int argc, char **argv)
   service_status.dwCurrentState = SERVICE_RUNNING;
   service_fns.SetServiceStatus_fn(hStatus, &service_status);
   set_main_thread();
-  do_main_loop();
+  run_tor_main_loop();
   tor_cleanup();
 }
 
