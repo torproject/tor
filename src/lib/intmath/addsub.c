5f76a0ca769dc009cd05a1c8dc19959f5e805107
teor <teor@torproject.org>
2019-01-08 19:22:02 +1000

    intmath: refactor tor_add_{u32,u64}_nowrap using a macro
    This change reduces code duplication, but costs readability
    (and might make it harder for some code tools).
    
    Preparation for 29005.

--- src/lib/intmath/addsub.c
+++ src/lib/intmath/addsub.c
@@ -14,28 +14,25 @@
 #include "lib/intmath/addsub.h"
 #include "lib/cc/compat_compiler.h"
 
+/* A macro that implements a function to safely add two uintn_t's, capping at
+ * UINTN_MAX rather than overflow. Does not work for signed types, because
+ * (INTN_MAX - b) can underflow. */
+#define IMPLEMENT_ADD_NOWRAP(uintn_t, fn_name, UINTN_MAX) \
+uintn_t                                                   \
+fn_name(uintn_t a, uintn_t b)                             \
+{                                                         \
+  /* a+b > UINTN_MAX check, without overflow */           \
+  if (PREDICT_UNLIKELY(a > UINTN_MAX - b)) {              \
+    return UINTN_MAX;                                     \
+  } else {                                                \
+    return a+b;                                           \
+  }                                                       \
+}
+
 /* Helper: safely add two uint32_t's, capping at UINT32_MAX rather
  * than overflow */
-uint32_t
-tor_add_u32_nowrap(uint32_t a, uint32_t b)
-{
-  /* a+b > UINT32_MAX check, without overflow */
-  if (PREDICT_UNLIKELY(a > UINT32_MAX - b)) {
-    return UINT32_MAX;
-  } else {
-    return a+b;
-  }
-}
+IMPLEMENT_ADD_NOWRAP(uint32_t, tor_add_u32_nowrap, UINT32_MAX)
 
 /* Helper: safely add two uint64_t's, capping at UINT64_MAX rather
  * than overflow */
-uint64_t
-tor_add_u64_nowrap(uint64_t a, uint64_t b)
-{
-  /* a+b > UINT64_MAX check, without overflow */
-  if (PREDICT_UNLIKELY(a > UINT64_MAX - b)) {
-    return UINT64_MAX;
-  } else {
-    return a+b;
-  }
-}
+IMPLEMENT_ADD_NOWRAP(uint64_t, tor_add_u64_nowrap, UINT64_MAX)
