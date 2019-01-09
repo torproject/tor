2d3c600915c22f1e9a7e9dcbba8358556ef64505
David Goulet <dgoulet@torproject.org>
2019-01-09 10:39:58 -0500

    sendme: Add helper functions for DATA cell delivery
    When we get a relay DATA cell delivered, we have to decrement the deliver
    window on both the circuit and stream level.
    
    This commit adds helper functions to handle the deliver window decrement.
    
    Part of #26840
    
    Signed-off-by: David Goulet <dgoulet@torproject.org>

--- src/core/or/sendme.h
+++ src/core/or/sendme.h
@@ -13,13 +13,19 @@
 #include "core/or/crypt_path_st.h"
 #include "core/or/circuit_st.h"
 
+/* Sending SENDME cell. */
 void sendme_connection_edge_consider_sending(edge_connection_t *edge_conn);
 void sendme_circuit_consider_sending(circuit_t *circ,
                                      crypt_path_t *layer_hint);
 
+/* Processing SENDME cell. */
 int sendme_process_circuit_level(crypt_path_t *layer_hint,
                                  circuit_t *circ, uint16_t cell_body_len);
 int sendme_process_stream_level(edge_connection_t *conn, circuit_t *circ,
                                 uint16_t cell_body_len);
+
+/* Update deliver window functions. */
+int sendme_stream_data_received(edge_connection_t *conn);
+int sendme_circuit_data_received(circuit_t *circ, crypt_path_t *layer_hint);
 
 #endif /* !defined(TOR_SENDME_H) */
