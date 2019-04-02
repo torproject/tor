de70eebc65d40d50f877b0f82df4d05ce670faa5
Nick Mathewson <nickm@torproject.org>
2019-04-02 08:17:22 -0400

    Start on a command-parsing tool for controller commands.
    There _is_ an underlying logic to these commands, but it isn't
    wholly uniform, given years of tweaks and changes.  Fortunately I
    think there is a superset that will work.
    
    This commit adds a parser for some of the most basic cases -- the
    ones currently handled by getargs_helper() and some of the
    object-taking ones.  Soon will come initial tests; then I'll start using
    the parser.
    
    After that, I'll expand the parser to handle the other cases that come
    up in the controller protocol.

---
+++ src/feature/control/control_cmd_args_st.h
@@ -0,0 +1,44 @@
+/* Copyright (c) 2001 Matej Pfajfar.
+ * Copyright (c) 2001-2004, Roger Dingledine.
+ * Copyright (c) 2004-2006, Roger Dingledine, Nick Mathewson.
+ * Copyright (c) 2007-2019, The Tor Project, Inc. */
+/* See LICENSE for licensing information */
+
+/**
+ * \file control_cmd_args_st.h
+ * \brief Definition for control_cmd_args_t
+ **/
+
+#ifndef TOR_CONTROL_CMD_ST_H
+#define TOR_CONTROL_CMD_ST_H
+
+struct smartlist_t;
+struct config_line_t;
+
+/**
+ * Parsed arguments for a control command.
+ *
+ * WORK IN PROGRESS: This structure is going to get more complex as this
+ * branch goes on.
+ **/
+struct control_cmd_args_t {
+  /**
+   * The command itself, as provided by the controller.  Not owned by this
+   * structure.
+   **/
+  const char *command;
+  /**
+   * Positional arguments to the command.
+   **/
+  struct smartlist_t *args;
+  /**
+   * Number of bytes in <b>object</b>; 0 if <b>object</b> is not set.
+   **/
+  size_t object_len;
+  /**
+   * A multiline object passed with this command.
+   **/
+  char *object;
+};
+
+#endif /* !defined(TOR_CONTROL_CMD_ST_H) */
