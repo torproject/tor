973800b847844cfaacf48658d02fe3ada77cbcf6
teor <teor@torproject.org>
2019-06-11 14:29:10 +1000

    scripts/git: Stop hard-coding the bash path in the git scripts
    Some OSes don't have bash in /usr/bin, others have an ancient bash at
    this path.
    
    Fixes bug 30840; bugfix on 0.4.0.1-alpha.

--- scripts/git/git-merge-forward.sh
+++ scripts/git/git-merge-forward.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ##############################
 # Configuration (change me!) #
--- scripts/git/git-pull-all.sh
+++ scripts/git/git-pull-all.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ##################################
 # User configuration (change me) #
