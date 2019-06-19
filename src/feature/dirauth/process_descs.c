bb15332d6fb4f72f4ab532ba4a03bf0526ac2e28
Neel Chauhan <neel@neelc.org>
2019-06-19 12:58:42 -0400

    Add our own base-64 encoded ed25519 public key in dirserv_add_own_fingerprint()

--- src/feature/dirauth/process_descs.c
+++ src/feature/dirauth/process_descs.c
@@ -154,7 +154,7 @@ add_ed25519_to_dir(const ed25519_public_key_t *edkey, authdir_config_t *list,
 /** Add the fingerprint for this OR to the global list of recognized
  * identity key fingerprints. */
 int
-dirserv_add_own_fingerprint(crypto_pk_t *pk)
+dirserv_add_own_fingerprint(crypto_pk_t *pk, const ed25519_public_key_t *edkey)
 {
   char fp[FINGERPRINT_LEN+1];
   if (crypto_pk_get_fingerprint(pk, fp, 0)<0) {
@@ -164,6 +164,7 @@ dirserv_add_own_fingerprint(crypto_pk_t *pk)
   if (!fingerprint_list)
     fingerprint_list = authdir_config_new();
   add_rsa_fingerprint_to_dir(fp, fingerprint_list, 0);
+  add_ed25519_to_dir(edkey, fingerprint_list, 0);
   return 0;
 }
 
