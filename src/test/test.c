adf70c475329604de09c0df5d351346cd44f0f9b
Neel Chauhan <neel@neelc.org>
2019-06-25 10:56:27 -0400

    Add test for rejecting duplicate v2 intro point descriptors

--- src/test/test.c
+++ src/test/test.c
@@ -639,6 +639,90 @@ test_rend_fns(void *arg)
   tor_free(intro_points_encrypted);
 }
 
+/** Test parsing of duplicate rendezvous service descriptors. */
+static void
+test_rend_dup(void *arg)
+{
+  rend_service_descriptor_t *generated = NULL, *parsed = NULL;
+  char service_id[DIGEST_LEN];
+  char service_id_base32[REND_SERVICE_ID_LEN_BASE32+1];
+  const char *next_desc;
+  smartlist_t *descs = smartlist_new();
+  char computed_desc_id[DIGEST_LEN];
+  char parsed_desc_id[DIGEST_LEN];
+  crypto_pk_t *pk1 = NULL, *pk2 = NULL;
+  time_t now;
+  char *intro_points_encrypted = NULL;
+  size_t intro_points_size;
+  size_t encoded_size;
+  int i;
+
+  (void)arg;
+
+  /* Initialize the service cache. */
+  rend_cache_init();
+
+  pk1 = pk_generate(0);
+  pk2 = pk_generate(1);
+  generated = tor_malloc_zero(sizeof(rend_service_descriptor_t));
+  generated->pk = crypto_pk_dup_key(pk1);
+  crypto_pk_get_digest(generated->pk, service_id);
+  base32_encode(service_id_base32, REND_SERVICE_ID_LEN_BASE32+1,
+                service_id, REND_SERVICE_ID_LEN);
+  now = time(NULL);
+  generated->timestamp = now;
+  generated->version = 2;
+  generated->protocols = 42;
+  generated->intro_nodes = smartlist_new();
+
+  crypto_pk_t *okey = pk_generate(2);
+
+  for (i = 0; i < 3; i++) {
+    rend_intro_point_t *intro = tor_malloc_zero(sizeof(rend_intro_point_t));
+    intro->extend_info = tor_malloc_zero(sizeof(extend_info_t));
+    intro->extend_info->onion_key = crypto_pk_dup_key(okey);
+    crypto_pk_get_digest(intro->extend_info->onion_key,
+                         intro->extend_info->identity_digest);
+    intro->extend_info->nickname[0] = '$';
+    base16_encode(intro->extend_info->nickname + 1,
+                  sizeof(intro->extend_info->nickname) - 1,
+                  intro->extend_info->identity_digest, DIGEST_LEN);
+
+    /* Use a fixed IP and port */
+    tor_addr_from_ipv4h(&intro->extend_info->addr, 0x60F6C5F7);
+    intro->extend_info->port = 9001;
+    intro->intro_key = crypto_pk_dup_key(pk2);
+
+    smartlist_add(generated->intro_nodes, intro);
+  }
+
+  crypto_pk_free(okey);
+
+  rend_encode_v2_descriptors(descs, generated, now, 0,
+                             REND_NO_AUTH, NULL, NULL);
+  rend_compute_v2_desc_id(computed_desc_id, service_id_base32, NULL, now, 0);
+  rend_parse_v2_service_descriptor(&parsed, parsed_desc_id,
+               &intro_points_encrypted, &intro_points_size, &encoded_size,
+               &next_desc,
+          ((rend_encoded_v2_service_descriptor_t *)smartlist_get(descs, 0))
+                                        ->desc_str, 1);
+  tt_int_op(rend_parse_introduction_points(parsed, intro_points_encrypted,
+                                         intro_points_size),OP_EQ, -1);
+
+  rend_service_descriptor_free(parsed);
+
+ done:
+  for (i = 0; i < smartlist_len(descs); i++)
+    rend_encoded_v2_service_descriptor_free_(smartlist_get(descs, i));
+  smartlist_free(descs);
+
+  rend_service_descriptor_free(parsed);
+  rend_service_descriptor_free(generated);
+  crypto_pk_free(pk1);
+  crypto_pk_free(pk2);
+  tor_free(intro_points_encrypted);
+}
+
 /** Run unit tests for stats code. */
 static void
 test_stats(void *arg)
@@ -812,6 +896,7 @@ static struct testcase_t test_array[] = {
   { "fast_handshake", test_fast_handshake, 0, NULL, NULL },
   FORK(circuit_timeout),
   FORK(rend_fns),
+  FORK(rend_dup),
   FORK(stats),
 
   END_OF_TESTCASES
