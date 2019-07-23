1bc22a122ce22e35cf9bcdbe860a6c2accd7ab27
Neel Chauhan <neel@neelc.org>
2019-07-23 14:43:26 -0400

    fixup! Check for private IPv6 addresses in circuit_extend()

--- src/core/or/circuitbuild.c
+++ src/core/or/circuitbuild.c
@@ -1250,8 +1250,8 @@ circuit_extend(cell_t *cell, circuit_t *circ)
     return -1;
   }
 
-  if (tor_addr_is_internal(&ec.orport_ipv4.addr, 0) &&
-      tor_addr_is_internal(&ec.orport_ipv6.addr, 0) &&
+  if ((tor_addr_is_internal(&ec.orport_ipv4.addr, 0) ||
+       tor_addr_is_internal(&ec.orport_ipv6.addr, 0)) &&
       !get_options()->ExtendAllowPrivateAddresses) {
     log_fn(LOG_PROTOCOL_WARN, LD_PROTOCOL,
            "Client asked me to extend to a private address");
