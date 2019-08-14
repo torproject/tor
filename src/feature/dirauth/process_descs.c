cdd5ce3502939e819f942fa48978a921477fdd38
Neel Chauhan <neel@neelc.org>
2019-08-13 20:35:48 -0400

    fixup! Check for private IPv6 addresses in dirserv_router_has_valid_address()

--- src/feature/dirauth/process_descs.c
+++ src/feature/dirauth/process_descs.c
@@ -436,10 +436,12 @@ dirserv_router_has_valid_address(routerinfo_t *ri)
     return 0; /* whatever it is, we're fine with it */
   tor_addr_from_ipv4h(&addr, ri->addr);
 
+  /* We only check internal v6 on non-null addresses because we do not require
+   * IPv6 and null IPv6 is normal. */
   if (tor_addr_is_internal(&addr, 0) ||
       tor_addr_is_null(&addr) ||
-      (tor_addr_is_internal(&ri->ipv6_addr, 0) && /* Only check internal v6 */
-       !tor_addr_is_null(&ri->ipv6_addr))) {      /* on non-null addresses. */
+      (tor_addr_is_internal(&ri->ipv6_addr, 0) &&
+       !tor_addr_is_null(&ri->ipv6_addr))) {
     log_info(LD_DIRSERV,
              "Router %s published internal IP address. Refusing.",
              router_describe(ri));
