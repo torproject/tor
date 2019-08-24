65511a45adfeff258e71329b3ff98d5159f09f88
Nick Mathewson <nickm@torproject.org>
2019-08-24 17:26:02 -0400

    Fix comment: ___ options are now that way because of the INVISIBLE flag.

--- src/app/config/config.c
+++ src/app/config/config.c
@@ -8136,7 +8136,7 @@ getinfo_helper_config(control_connection_t *conn,
     int i;
     for (i = 0; option_vars_[i].member.name; ++i) {
       const config_var_t *var = &option_vars_[i];
-      /* don't tell controller about triple-underscore options */
+      /* don't tell controller about invisible options */
       if (option_vars_[i].flags & CVFLAG_INVISIBLE)
         continue;
       const char *type = struct_var_get_typename(&var->member);
