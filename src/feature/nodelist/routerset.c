04ab357df80582d3d9e7a78471e051f8f774d27b
Nick Mathewson <nickm@torproject.org>
2019-08-28 11:39:31 -0400

    Remove extraneous "n" from comment in routerset.c

--- src/feature/nodelist/routerset.c
+++ src/feature/nodelist/routerset.c
@@ -1,5 +1,5 @@
 /* Copyright (c) 2001 Matej Pfajfar.
-n * Copyright (c) 2001-2004, Roger Dingledine.
+ * Copyright (c) 2001-2004, Roger Dingledine.
  * Copyright (c) 2004-2006, Roger Dingledine, Nick Mathewson.
  * Copyright (c) 2007-2019, The Tor Project, Inc. */
 /* See LICENSE for licensing information */
