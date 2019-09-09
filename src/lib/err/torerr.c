ef4addfa79767a7144e342657e72c4cc91d6848e
teor <teor@torproject.org>
2019-09-09 13:35:40 +1000

    fixup! log: Don't close file log fds that are being used by the err module

--- src/lib/err/torerr.c
+++ src/lib/err/torerr.c
@@ -115,7 +115,7 @@ tor_log_get_sigsafe_err_fds(const int **out)
  * they should remain open even while logs are being reconfigured.) Therefore,
  * any fds closed by the log module should be dup()ed, and the duplicate fd
  * should be given to the err module in fds. In particular, the log module
- * owns file log fds, but does not own stdio log fds.
+ * closes the file log fds, but does not close the stdio log fds.
  *
  * If fds is NULL or n is 0, clears the list of error fds.
  */
