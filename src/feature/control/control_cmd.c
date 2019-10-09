54c5366aa15d667484a93abf16d166db7981a0ea
Nick Mathewson <nickm@torproject.org>
2019-10-09 12:36:47 -0400

    feature/control: wrap some problem macros in COCCI

--- src/feature/control/control_cmd.c
+++ src/feature/control/control_cmd.c
@@ -2245,6 +2245,7 @@ typedef struct control_cmd_def_t {
  */
 #define CMD_FL_WIPE (1u<<0)
 
+#ifndef COCCI
 /** Macro: declare a command with a one-line argument, a given set of flags,
  * and a syntax definition.
  **/
@@ -2277,6 +2278,7 @@ typedef struct control_cmd_def_t {
       0,                                        \
       &obsolete_syntax,                         \
   }
+#endif
 
 /**
  * An array defining all the recognized controller commands.
