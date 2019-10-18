5c19215d01ffec0eb0a6ba790a314d23dab0f25c
teor <teor@torproject.org>
2019-10-18 18:00:00 +1000

    test: Skip test scripts that require the relay module
    When tor is built without the relay module.
    
    Part of 32123.

--- src/test/test_zero_length_keys.sh
+++ src/test/test_zero_length_keys.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # Check that tor regenerates keys when key files are zero-length
 
+if "${builddir:-.}/src/app/tor" --list-modules | grep -q "relay: no"; then
+  echo "This test requires the relay module. Skipping." >&2
+  exit 77
+fi
+
 exitcode=0
 
 "${SHELL:-sh}" "${abs_top_srcdir:-.}/src/test/zero_length_keys.sh" "${builddir:-.}/src/app/tor" -z || exitcode=1
