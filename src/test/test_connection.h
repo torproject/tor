b6b7ff1f67fad9c3f75e749cab3979adb61e7219
Suphanat Chunhapanya <haxx.pop@gmail.com>
2019-10-21 18:00:02 +0800

    test: HAPRoxy protocol

--- src/test/test_connection.h
+++ src/test/test_connection.h
@@ -7,6 +7,7 @@
 /** Some constants used by test_connection and helpers */
 #define TEST_CONN_FAMILY        (AF_INET)
 #define TEST_CONN_ADDRESS       "127.0.0.1"
+#define TEST_CONN_ADDRESS_2     "127.0.0.2"
 #define TEST_CONN_PORT          (12345)
 #define TEST_CONN_ADDRESS_PORT  "127.0.0.1:12345"
 #define TEST_CONN_FD_INIT 50
