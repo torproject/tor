616bc21859c50f75e0f1092cab0dca0de62f47b9
teor <teor@torproject.org>
2019-10-23 15:40:09 +1000

    scripts/cocci: Add check_cocci_parse.sh and default exceptions
    Add check_cocci_parse.sh, which is a thin wrapper around
    try_parse.sh. Add a default exceptions file, and use it in
    check_cocci_parse.sh.
    
    Part of 31919.

---
+++ scripts/coccinelle/check_cocci_parse.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# If we have coccinelle installed, run try_parse.sh on every filename passed
+# as an argument. Uses the default coccinelle exceptions file, if
+# $TOR_COCCI_EXCEPTIONS_FILE is not set.
+#
+# Use TOR_COCCI_EXCEPTIONS_FILE=/dev/null check_cocci_parse.sh to disable
+# the default exception file.
+#
+# If spatch is not installed, remind the user to install it, but exit with
+# a success error status.
+
+scripts_cocci="$(dirname "$0")"
+top="$scripts_cocci/../.."
+try_parse="$scripts_cocci/try_parse.sh"
+
+exitcode=0
+
+export TOR_COCCI_EXCEPTIONS_FILE="${TOR_COCCI_EXCEPTIONS_FILE:-$scripts_cocci/exceptions.txt}"
+
+if ! command -v spatch; then
+    echo "Install coccinelle's spatch to check cocci C parsing!"
+    exit "$exitcode"
+fi
+
+"$try_parse" "$@"
+exitcode=$?
+
+if test "$exitcode" != 0 ; then
+    echo "Please fix these cocci parsing errors"
+    echo "Try using test-operator-cleanup or 'make autostyle-operators'"
+    echo "As a last resort, you can modify scripts/coccinelle/exceptions.txt"
+fi
+
+exit "$exitcode"
