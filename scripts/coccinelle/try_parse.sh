abe3fcf488560494ab1ee314c0b157c118916257
teor <teor@torproject.org>
2019-10-23 15:37:20 +1000

    scripts/cocci: Add an exceptions file to try_parse.sh
    Set $TOR_COCCI_EXCEPTIONS_FILE to the name of an exceptions file,
    containing a list of filename patterns matched by 'grep -f'.
    
    Part of 31919.

--- scripts/coccinelle/try_parse.sh
+++ scripts/coccinelle/try_parse.sh
@@ -2,12 +2,27 @@
 
 # Echo the name of every argument of this script that is not "perfect"
 # according to coccinelle's --parse-c.
+#
+# If $TOR_COCCI_EXCEPTIONS_FILE is non-empty, skip any files that match the
+# patterns in the exception file, according to "grep -f"
+#
+# If VERBOSE is non-empty, log spatch errors and skipped files.
 
 top="$(dirname "$0")/../.."
 
 exitcode=0
 
 for fn in "$@"; do
+
+    if test "${TOR_COCCI_EXCEPTIONS_FILE}" ; then
+        skip_fn=$(echo "$fn" | grep -f "${TOR_COCCI_EXCEPTIONS_FILE}")
+        if test "${skip_fn}" ; then
+            if test "${VERBOSE}" != ""; then
+                echo "Skipping '${skip_fn}'"
+            fi
+            continue
+        fi
+    fi
 
     if spatch -macro_file_builtins "$top"/scripts/coccinelle/tor-coccinelle.h \
               -I "$top" -I "$top"/src -I "$top"/ext --defined COCCI \
