733c193a9f25e7e407a574a7d1ede77674b918a3
Nick Mathewson <nickm@torproject.org>
2019-10-24 07:48:49 -0400

    fixup! Add a script to rename C identifiers, and optionally add a commit message
    Add a copyright/license statement.

--- scripts/maint/rename_c_identifier.py
+++ scripts/maint/rename_c_identifier.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+#
+# Copyright (c) 2001 Matej Pfajfar.
+# Copyright (c) 2001-2004, Roger Dingledine.
+# Copyright (c) 2004-2006, Roger Dingledine, Nick Mathewson.
+# Copyright (c) 2007-2019, The Tor Project, Inc.
+# See LICENSE for licensing information
 
 """
 Helpful script to replace one or more C identifiers, and optionally
