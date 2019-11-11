cd8c96ce02735db83c241c26e06b9bab50a0dae4
Nick Mathewson <nickm@torproject.org>
2019-11-11 16:31:50 -0500

    Typo fix in warning message about ControlSocket

--- src/app/config/config.c
+++ src/app/config/config.c
@@ -3400,7 +3400,7 @@ options_validate_cb(const void *old_options_, void *options_, char **msg)
   }
 #else /* defined(HAVE_SYS_UN_H) */
   if (options->ControlSocketsGroupWritable && !options->ControlSocket) {
-    *msg = tor_strdup("Setting ControlSocketGroupWritable without setting"
+    *msg = tor_strdup("Setting ControlSocketGroupWritable without setting "
                       "a ControlSocket makes no sense.");
     return -1;
   }
