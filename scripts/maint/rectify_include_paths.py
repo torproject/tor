cf7281e906a46533d27e24665342b923e70603f1
teor <teor@torproject.org>
2019-12-02 12:25:18 +1000

    rectify-includes: Fix python function comment spacing

--- scripts/maint/rectify_include_paths.py
+++ scripts/maint/rectify_include_paths.py
@@ -45,7 +45,9 @@ DEFAULT_FIX_MODE = FIX_DUPLICATES
 ## Command-line arguments
 
 def parse_arguments(argv=sys.argv):
-    '''Parse the command-line arguments.'''
+    '''
+    Parse the command-line arguments.
+    '''
     parser = argparse.ArgumentParser(
         description="Reformat tor header includes into a consistent style.")
 
@@ -128,27 +130,31 @@ current_file = None
 ## Functions that access globals
 
 def set_debug_mode(new_debug_mode=True):
-    '''Set the global debug_mode to new_debug_mode.
-       '''
+    '''
+    Set the global debug_mode to new_debug_mode.
+    '''
     global debug_mode
     debug_mode = new_debug_mode
 
 def get_debug_mode():
-    '''Get the global debug_mode.
-       '''
+    '''
+    Get the global debug_mode.
+    '''
     global debug_mode
     return debug_mode
 
 def set_current_file(new_current_file):
-    '''Set the global current_file to new_current_file.
-       '''
+    '''
+    Set the global current_file to new_current_file.
+    '''
     global current_file
     current_file = new_current_file
 
 def format_current_file():
-    '''Format the global current_file, handling None correctly.
-       Returns a string.
-       '''
+    '''
+    Format the global current_file, handling None correctly.
+    Returns a string.
+    '''
     global current_file
     if current_file:
         return " in '{}'".format(current_file)
@@ -158,13 +164,17 @@ def format_current_file():
 ## Other logging functions
 
 def debug(msg):
-    '''Log debug to stdout with msg, if get_debug_mode() is True.'''
+    '''
+    Log debug to stdout with msg, if get_debug_mode() is True.
+    '''
     if get_debug_mode():
         sys.stdout.write("DEBUG: {}\n".format(msg,
                                               format_current_file()))
 
 def debug_map(map_dict, name):
-    '''Log debug map_dict called name.'''
+    '''
+    Log debug map_dict called name.
+    '''
     # skip iteration and formatting on large maps
     if get_debug_mode():
         debug("{}:".format(name))
@@ -172,18 +182,21 @@ def debug_map(map_dict, name):
             debug("{}: {}".format(key, map_dict[key]))
 
 def level_for_exit_error(exit_error):
-    '''Return the default level for exit_error.'''
+    '''
+    Return the default level for exit_error.
+    '''
     if exit_error:
         return "ERROR"
     else:
         return "WARNING"
 
 def warn(msg, exit_error=None, log_level=None):
-    '''Log msg and current_file to stderr.
-       If log_level, log at that level, otherwise, check exit_error.
-       If exit_error, log an error, and exit with that status.
-       Otherwise, log a warning.
-       '''
+    '''
+    Log msg and current_file to stderr.
+    If log_level, log at that level, otherwise, check exit_error.
+    If exit_error, log an error, and exit with that status.
+    Otherwise, log a warning.
+    '''
     if not log_level:
         log_level = level_for_exit_error(exit_error)
     sys.stderr.write("{}: {}{}\n".format(log_level,
@@ -197,10 +210,11 @@ def warn_with_context(line,
                       context="",
                       exit_error=None,
                       log_level=None):
-    '''Warn about action on line, in context.
-       context is optional.
-       Handle exit_error and log_level like warn().
-       '''
+    '''
+    Warn about action on line, in context.
+    context is optional.
+    Handle exit_error and log_level like warn().
+    '''
     if not log_level:
         log_level = level_for_exit_error(exit_error)
     if context:
@@ -215,10 +229,11 @@ def warn_ignored(line,
                  context="",
                  exit_error=None,
                  log_level=None):
-    '''Warn about ignoring line in context.
-       context is optional.
-       Pass exit_error and log_level to warn_with_context().
-       '''
+    '''
+    Warn about ignoring line in context.
+    context is optional.
+    Pass exit_error and log_level to warn_with_context().
+    '''
     warn_with_context(line,
                       action="Ignoring",
                       context=context,
@@ -231,13 +246,14 @@ def warn_ignored(line,
 DUPLICATE = object()
 
 def exclude(paths, dirpath, dirnames):
-    '''Remove any strings from dirnames that are equal to an item in paths.
-       If dirpath is equal to an item in paths, clear dirnames,
-       and return True. os.walk() will not scan any deeper in this directory,
-       and the caller should skip all fnames at this level.
-       Otherwise, return False. The caller should continue using fnames
-       and using os.walk() to scan the tree.
-       '''
+    '''
+    Remove any strings from dirnames that are equal to an item in paths.
+    If dirpath is equal to an item in paths, clear dirnames,
+    and return True. os.walk() will not scan any deeper in this directory,
+    and the caller should skip all fnames at this level.
+    Otherwise, return False. The caller should continue using fnames
+    and using os.walk() to scan the tree.
+    '''
     for p in paths:
         if os.path.join(SRC_ROOT, p) == dirpath:
             del dirnames[:]
@@ -247,13 +263,16 @@ def exclude(paths, dirpath, dirnames):
     return False
 
 def get_base_file_name(hdr):
-    '''Return the file name part of the path hdr.'''
+    '''
+    Return the file name part of the path hdr.
+    '''
     return os.path.split(hdr)[1]
 
 def get_private_from_if_pat(line, pat_list=IF_PRIVATE_PAT_LIST):
-    '''If any pattern in pat_list matches, return a string containing the
-       PRIVATE macro name. Otherwise, return None.
-       '''
+    '''
+    If any pattern in pat_list matches, return a string containing the
+    PRIVATE macro name. Otherwise, return None.
+    '''
     for pat in pat_list:
         m = pat.match(line)
         if m:
@@ -262,9 +281,10 @@ def get_private_from_if_pat(line, pat_list=IF_PRIVATE_PAT_LIST):
     return None
 
 def get_private_from_define_pat(line, pat_list=DEFINE_PRIVATE_PAT_LIST):
-    '''If any pattern in pat_list matches, return a string containing
-       the PRIVATE macro name. Otherwise, return None.
-       '''
+    '''
+    If any pattern in pat_list matches, return a string containing
+    the PRIVATE macro name. Otherwise, return None.
+    '''
     for pat in pat_list:
         m = pat.match(line)
         if m:
@@ -273,9 +293,10 @@ def get_private_from_define_pat(line, pat_list=DEFINE_PRIVATE_PAT_LIST):
     return None
 
 def get_fname_from_include_pat(line, pat=INCLUDE_H_FNAME_PAT):
-    '''If pat matches, return a string containing the header name.
-       Otherwise, return None.
-       '''
+    '''
+    If pat matches, return a string containing the header name.
+    Otherwise, return None.
+    '''
     m = pat.match(line)
     if m:
         _, dirname, fname, _ = m.groups()
@@ -283,10 +304,11 @@ def get_fname_from_include_pat(line, pat=INCLUDE_H_FNAME_PAT):
     return None
 
 def add_include_to_map(fullname, fname, inc_map):
-    '''If fullname is the first path for fname, add it to inc_map,
-       and return True. Otherwise, add a duplicate marker to inc_map,
-       and return False.
-       '''
+    '''
+    If fullname is the first path for fname, add it to inc_map,
+    and return True. Otherwise, add a duplicate marker to inc_map,
+    and return False.
+    '''
     assert fullname.startswith(SRC_ROOT + "/")
     include = fullname[4:]
     if fname in inc_map:
@@ -299,15 +321,18 @@ def add_include_to_map(fullname, fname, inc_map):
         return True
 
 def add_to_set_map(set_map, key, value):
-    '''Add value to the set in set_map for key.'''
+    '''
+    Add value to the set in set_map for key.
+    '''
     if key not in set_map:
         set_map[key] = set()
     set_map[key].add(value)
 
 def add_private_to_map(fullname, fname, priv_inc_map):
-    '''If fullname uses a PRIVATE macro, and it is the first header
-       to do so, add it to priv_inc_map.
-       '''
+    '''
+    If fullname uses a PRIVATE macro, and it is the first header
+    to do so, add it to priv_inc_map.
+    '''
     with open(fullname, 'r') as header:
         for line in header:
             priv_name = get_private_from_if_pat(line)
@@ -319,10 +344,11 @@ def add_private_to_map(fullname, fname, priv_inc_map):
                 add_to_set_map(priv_inc_map, priv_name, fname)
 
 def get_include_private_maps():
-    '''Find all the include files, map them to their real names.
-       Also map each PRIVATE directive to the set of headers
-       that use it.
-       '''
+    '''
+    Find all the include files, map them to their real names.
+    Also map each PRIVATE directive to the set of headers
+    that use it.
+    '''
     inc_map = { }
     priv_inc_map = { }
 
@@ -350,14 +376,15 @@ def get_include_private_maps():
 ## Fixing lines
 
 def fix_line(line, pat, std_directive=None, inc_map=None):
-    '''If line matches pat, reformat it into a canonical string.
-       Optionally, use std_directive instead of the first part of pat.
-       Optionally, use inc_map to lookup the canonical form of the second
-       part of pat, and modify the line if the lookup succeeds.
-       Leave the third part of pat unmodified.
-       If pat matches, returns a possibly modified line,
-       otherwise returns None.
-       '''
+    '''
+    If line matches pat, reformat it into a canonical string.
+    Optionally, use std_directive instead of the first part of pat.
+    Optionally, use inc_map to lookup the canonical form of the second
+    part of pat, and modify the line if the lookup succeeds.
+    Leave the third part of pat unmodified.
+    If pat matches, returns a possibly modified line,
+    otherwise returns None.
+    '''
     m = pat.match(line)
     if m:
         directive, prefix, rest = m.groups()
@@ -372,12 +399,13 @@ def fix_line(line, pat, std_directive=None, inc_map=None):
     return None
 
 def fix_macro_cond_line(line, pat):
-    '''If line matches pat, reformat it into a canonical string.
-       Parts 1 and 3 are whitespace, move them after part 2, which is '#'.
-       Then leave part 4 umodified.
-       If pat matches, returns a possibly modified line,
-       otherwise returns None.
-       '''
+    '''
+    If line matches pat, reformat it into a canonical string.
+    Parts 1 and 3 are whitespace, move them after part 2, which is '#'.
+    Then leave part 4 umodified.
+    If pat matches, returns a possibly modified line,
+    otherwise returns None.
+    '''
     m = pat.match(line)
     if m:
         s1,  start,  s2,  rest = m.groups()
@@ -385,10 +413,11 @@ def fix_macro_cond_line(line, pat):
     return None
 
 def fix_private(line, pat_list=DEFINE_PRIVATE_PAT_LIST):
-    '''Fix PRIVATE defines in line, using the first matching pattern in
-       pat_list. If the line was matched, returns a possibly modified line,
-       otherwise returns None.
-       '''
+    '''
+    Fix PRIVATE defines in line, using the first matching pattern in
+    pat_list. If the line was matched, returns a possibly modified line,
+    otherwise returns None.
+    '''
     for pat in pat_list:
         mod_line = fix_line(line,
                             pat,
@@ -399,11 +428,12 @@ def fix_private(line, pat_list=DEFINE_PRIVATE_PAT_LIST):
     return None
 
 def fix_cond(line):
-    '''Fix #if, #ifdef, #elif, #else, and #endif macro directives in line.
-       If pat matches, returns a pair with a possibly modified line,
-       and a nesting increment.
-       Otherwise, returns None.
-       '''
+    '''
+    Fix #if, #ifdef, #elif, #else, and #endif macro directives in line.
+    If pat matches, returns a pair with a possibly modified line,
+    and a nesting increment.
+    Otherwise, returns None.
+    '''
     # match #if *
     mod_line = fix_macro_cond_line(line,
                                    IF_PAT)
@@ -437,11 +467,12 @@ def fix_cond(line):
     return (None, None)
 
 def fix_include_h(line, inc_map):
-    '''Fix #include .h macro directives in line, and write to private_groups
-       Use inc_map to map headers to canonical names.
-       If the line was matched, returns a possibly modified line,
-       otherwise returns None.
-       '''
+    '''
+    Fix #include .h macro directives in line, and write to private_groups
+    Use inc_map to map headers to canonical names.
+    If the line was matched, returns a possibly modified line,
+    otherwise returns None.
+    '''
     # match #include "*.h"*
     mod_line = fix_line(line,
                         INCLUDE_H_PAT,
@@ -453,11 +484,12 @@ def fix_include_h(line, inc_map):
     return None
 
 def fix_include_inc(line, inc_map):
-    '''Fix #include .inc macro directives in line.
-       Use inc_map to map headers to canonical names.
-       If pat matches, returns a possibly modified line,
-       otherwise returns None.
-       '''
+    '''
+    Fix #include .inc macro directives in line.
+    Use inc_map to map headers to canonical names.
+    If pat matches, returns a possibly modified line,
+    otherwise returns None.
+    '''
     # match #include "*.inc"*
     return fix_line(line,
                     INCLUDE_INC_PAT,
@@ -465,10 +497,11 @@ def fix_include_inc(line, inc_map):
                     inc_map=inc_map)
 
 def ignore_duplicate(key, used_set, line, context="duplicate"):
-    '''Check if key is already in used_set.
-       If it is, warn_ignored() using line and context, and return None.
-       Otherwise, add key to used_set, and return line.
-       '''
+    '''
+    Check if key is already in used_set.
+    If it is, warn_ignored() using line and context, and return None.
+    Otherwise, add key to used_set, and return line.
+    '''
     if key in used_set:
         warn_ignored(line,
                      context=context)
@@ -481,15 +514,16 @@ def fix_line_helper(line, inc_map,
                     priv_inc_map=None,
                     used_priv=None,
                     used_inc=None):
-    '''Fix line using inc_map, and return the result.
+    '''
+    Fix line using inc_map, and return the result.
 
-       If priv_inc_map is not None, check for unused PRIVATE defines.
-       If used_priv is not None, check for duplicate PRIVATE defines.
-       If used_inc is not None, check for duplicate includes.
+    If priv_inc_map is not None, check for unused PRIVATE defines.
+    If used_priv is not None, check for duplicate PRIVATE defines.
+    If used_inc is not None, check for duplicate includes.
 
-       Returns None if line is unused or duplicate.
-       Otherwise, returns a fixed version of line.
-       '''
+    Returns None if line is unused or duplicate.
+    Otherwise, returns a fixed version of line.
+    '''
     # Create placeholder variables for FIX_LINES mode
     if priv_inc_map is None:
         priv_inc_map = {}
@@ -544,11 +578,12 @@ def fix_line_helper(line, inc_map,
 ## Fixing files
 
 def fix_file_lines(inp, out, inc_map):
-    '''Fix every line in inp using inc_map, and write the result to out.
-       Rewrites lines, but keeps unmodified lines the same.
-       Does not delete any lines.
-       Implements FIX_LINES mode.
-       '''
+    '''
+    Fix every line in inp using inc_map, and write the result to out.
+    Rewrites lines, but keeps unmodified lines the same.
+    Does not delete any lines.
+    Implements FIX_LINES mode.
+    '''
     # Since we're not re-ordering any lines, we can just process
     # the file line-by-line.
     for line in inp:
@@ -562,18 +597,19 @@ def fix_file_lines(inp, out, inc_map):
         out.write(mod_line)
 
 def fix_file_duplicates(inp, out, inc_map, priv_inc_map):
-    '''Fix every line in inp using inc_map, and write the result to out.
+    '''
+    Fix every line in inp using inc_map, and write the result to out.
 
-       Rewrites lines, and removes duplicate includes and PRIVATE defines,
-       based on inc_map.keys() and priv_inc_map.keys().
-       Also removes unused PRIVATE defines, based on priv_inc_map.keys().
-       (And removes any double newlines created by the deletions.)
+    Rewrites lines, and removes duplicate includes and PRIVATE defines,
+    based on inc_map.keys() and priv_inc_map.keys().
+    Also removes unused PRIVATE defines, based on priv_inc_map.keys().
+    (And removes any double newlines created by the deletions.)
 
-       Any comments that are directly before deleted lines are retained.
-       The user should move or delete them manually.
+    Any comments that are directly before deleted lines are retained.
+    The user should move or delete them manually.
 
-       Implements FIX_DUPLICATES mode.
-       '''
+    Implements FIX_DUPLICATES mode.
+    '''
     # The private DEFINES that have been used already in this file
     used_priv = set()
     # The includes that have been used already in this file
@@ -606,9 +642,10 @@ def fix_file_duplicates(inp, out, inc_map, priv_inc_map):
 
 def fix_file(fix_mode, inp, out,
              inc_map=None, priv_inc_map=None):
-    '''Fix every line in inp using the mappings, and write the result to out.
-       Use fix_mode to limit the fixes applied to the file.
-       '''
+    '''
+    Fix every line in inp using the mappings, and write the result to out.
+    Use fix_mode to limit the fixes applied to the file.
+    '''
     if fix_mode == FIX_LINES:
         fix_file_lines(inp, out,
                        inc_map)
@@ -627,12 +664,13 @@ def fix_file(fix_mode, inp, out,
 def fix_tree(fix_mode,
              root, exclude_list=[], target_file_list=None,
              inc_map=None, priv_inc_map=None):
-    '''Fix the tree at root, using the fixes from fix_mode.
-       Exclude directories in exclude_list.
-       If target_file_list is not None, only fix files in that list.
-       The file names in the list must be the base name, not the path.
-       Use the supplied mappings to do the fixes.
-       '''
+    '''
+    Fix the tree at root, using the fixes from fix_mode.
+    Exclude directories in exclude_list.
+    If target_file_list is not None, only fix files in that list.
+    The file names in the list must be the base name, not the path.
+    Use the supplied mappings to do the fixes.
+    '''
     for dirpath, dirnames, fnames in os.walk(root):
 
         if exclude(exclude_list, dirpath, dirnames):
@@ -669,8 +707,9 @@ def fix_tree(fix_mode,
 ## Entry points
 
 def main(argv=sys.argv):
-    '''Main function entry point, using argument list argv.'''
-
+    '''
+    Main function entry point, using argument list argv.
+    '''
     args = parse_arguments(argv=argv)
     set_debug_mode(args.debug_mode)
     os.chdir(args.tor_dir)
