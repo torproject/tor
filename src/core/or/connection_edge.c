302e57632e5b79b12528402e5a36ad9cd72f5188
Moonsik Park <mooonsik.park@gmail.com>
2019-12-04 23:06:45 +0900

    Cast HiddenServiceExportInstanceID to uint16_t before printf.
    Co-Authored-By: teor <teor@torproject.org>

--- src/core/or/connection_edge.c
+++ src/core/or/connection_edge.c
@@ -902,7 +902,7 @@ export_hs_client_circuit_id(edge_connection_t *edge_conn,
   tor_asprintf(&buf, "PROXY TCP6 %s:%s:%04x:%04x:%04x %s %d %d\r\n",
                src_ipv6_prefix,
                rp_identity_buf ? rp_identity_buf : "0000:0000:0000",
-               get_options()->HiddenServiceExportInstanceID,
+               (uint16_t) (get_options()->HiddenServiceExportInstanceID),
                (uint16_t) (gid >> 16), (uint16_t) (gid & 0x0000ffff),
                dst_ipv6, src_port, dst_port);
 
