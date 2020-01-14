b34ee65a0065ed5a90c5f88d737bb1a5fa8bf807
David Goulet <dgoulet@torproject.org>
2020-01-14 14:58:09 -0500

    trace: Add USDT probes generation support
    This commit adds both configure options and probe generation for tracepoints.
    
    Part of #32910
    
    Signed-off-by: David Goulet <dgoulet@torproject.org>

--- src/lib/trace/events.h
+++ src/lib/trace/events.h
@@ -1,9 +1,9 @@
-/* Copyright (c) 2017-2020, The Tor Project, Inc. */
+/* Copyright (c) 2020, The Tor Project, Inc. */
 /* See LICENSE for licensing information */
 
 /**
  * \file events.h
- * \brief Header file for Tor event tracing.
+ * \brief Header file for Tor tracing instrumentation definition.
  **/
 
 #ifndef TOR_LIB_TRACE_EVENTS_H
@@ -14,16 +14,21 @@
 #ifdef HAVE_TRACING
 
 #define tor_trace(subsystem, event_name, args...) \
-  TOR_TRACE_LOG_DEBUG(subsystem, event_name);
+  TOR_TRACE_LOG_DEBUG(subsystem, event_name);     \
+  TOR_TRACE_USDT(subsystem, event_name, args);
 
 /* This corresponds to the --enable-tracing-instrumentation-log-debug
  * configure option which maps all tracepoints to a log_debug() statement. */
 #include "lib/trace/debug.h"
 
+/* This corresponds to the --enable-tracing-instrumentation-usdt configure
+ * option which will generate USDT probes for each tracepoints. */
+#include "lib/trace/usdt/usdt.h"
+
 #else /* !defined(HAVE_TRACING) */
 
-/* Reaching this point, we NOP every event declaration because event tracing
- * is not been enabled at compile time. */
+/* Reaching this point, tracing is disabled thus we NOP every tracepoints
+ * declaration so we have no execution cost at runtime. */
 #define tor_trace(subsystem, name, args...)
 
 #endif /* defined(HAVE_TRACING) */
