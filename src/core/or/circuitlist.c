0d61144b7fbbfbcc7634b04fee982f9c32c7f0ef
David Goulet <dgoulet@torproject.org>
2020-01-15 12:50:20 -0500

    trace: Add two circuit tracepoints for new circuits
    Signed-off-by: David Goulet <dgoulet@torproject.org>

--- src/core/or/circuitlist.c
+++ src/core/or/circuitlist.c
@@ -1082,6 +1082,7 @@ origin_circuit_new(void)
               prediction_time_remaining);
   }
 
+  tor_trace(circuit, new_origin, circ);
   return circ;
 }
 
@@ -1104,6 +1105,7 @@ or_circuit_new(circid_t p_circ_id, channel_t *p_chan)
 
   init_circuit_base(TO_CIRCUIT(circ));
 
+  tor_trace(circuit, new_or, circ);
   return circ;
 }
 
