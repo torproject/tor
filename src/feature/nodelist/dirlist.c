33414e5494b56f3f677d022bfb4bb1ed99191032
David Goulet <dgoulet@torproject.org>
2020-02-11 10:15:04 -0500

    test: Add unit test for connection_dir_is_global_write_low()
    Part of #33029
    
    Signed-off-by: David Goulet <dgoulet@torproject.org>

--- src/feature/nodelist/dirlist.c
+++ src/feature/nodelist/dirlist.c
@@ -66,8 +66,8 @@ add_trusted_dir_to_nodelist_addr_set(const dir_server_t *dir)
 
 /** Go over the trusted directory server list and add their address(es) to the
  * nodelist address set. This is called everytime a new consensus is set. */
-void
-dirlist_add_trusted_addresses(void)
+MOCK_IMPL(void,
+dirlist_add_trusted_addresses, (void))
 {
   if (!trusted_dir_servers) {
     return;
