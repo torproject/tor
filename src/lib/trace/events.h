9069cd670884601cf8157df5e4a2d5d0e19618e9
David Goulet <dgoulet@torproject.org>
2020-02-11 11:26:04 -0500

    trace: Add LTTng-UST interface support
    No probes at this point. They are per subsystem and thus in later commits.
    
    Part of #32910

--- src/lib/trace/events.h
+++ src/lib/trace/events.h
@@ -17,6 +17,7 @@
   do {                                                              \
     TOR_TRACE_LOG_DEBUG(tor_ ## subsystem, event_name);             \
     TOR_TRACE_USDT(tor_ ## subsystem, event_name, ## __VA_ARGS__);  \
+    TOR_TRACE_LTTNG(tor_ ## subsystem, event_name, ## __VA_ARGS__); \
   } while (0)
 
 /* This corresponds to the --enable-tracing-instrumentation-log-debug
@@ -26,6 +27,10 @@
 /* This corresponds to the --enable-tracing-instrumentation-usdt configure
  * option which will generate USDT probes for each tracepoints. */
 #include "lib/trace/usdt/usdt.h"
+
+/* This corresponds to the --enable-tracing-instrumentation-lttng configure
+ * option which will generate LTTng probes for each tracepoints. */
+#include "lib/trace/lttng/lttng.h"
 
 #else /* !defined(HAVE_TRACING) */
 
