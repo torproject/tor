0b242d7b9c8680c1b033b9c7b01d398bd91927cd
Nick Mathewson <nickm@torproject.org>
2020-02-14 13:29:15 -0500

    Move process subsystem after evloop.
    Process uses evloop, and so should be initialized after it.

--- src/lib/process/process_sys.c
+++ src/lib/process/process_sys.c
@@ -26,7 +26,7 @@ subsys_process_shutdown(void)
 
 const subsys_fns_t sys_process = {
   .name = "process",
-  .level = -35,
+  .level = -18,
   .supported = true,
   .initialize = subsys_process_initialize,
   .shutdown = subsys_process_shutdown
