2e80d7f1938d176e0e32725c090984a20eb0cc5d
Nick Mathewson <nickm@torproject.org>
2020-04-13 17:02:20 -0400

    bump to 0.4.3.4-rc-dev

--- src/win32/orconfig.h
+++ src/win32/orconfig.h
@@ -217,7 +217,7 @@
 #define USING_TWOS_COMPLEMENT
 
 /* Version number of package */
-#define VERSION "0.4.3.4-rc"
+#define VERSION "0.4.3.4-rc-dev"
 
 #define HAVE_STRUCT_SOCKADDR_IN6
 #define HAVE_STRUCT_IN6_ADDR
