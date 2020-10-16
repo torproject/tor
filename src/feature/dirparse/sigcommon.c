acf4225441fbb20eebedf32d15cb7ccd0488da9f
Nick Mathewson <nickm@torproject.org>
2020-10-16 09:26:41 -0400

    Fix up vote tests in test_dir_handle_get.
    These tests used a hardcoded vote with a hardcoded digest.  That
    vote didn't have any pr lines, and so it is now invalid. I've
    adjusted the testing code so that it mocks the signature-checking,
    so that we can more easily change the contents of the vote.

--- src/feature/dirparse/sigcommon.c
+++ src/feature/dirparse/sigcommon.c
@@ -139,13 +139,13 @@ signed_digest_equals, (const uint8_t *d1, const uint8_t *d2, size_t len))
  * the document when generating log messages.  Return 0 on success, negative
  * on failure.
  */
-int
-check_signature_token(const char *digest,
+MOCK_IMPL(int,
+check_signature_token,(const char *digest,
                       ssize_t digest_len,
                       directory_token_t *tok,
                       crypto_pk_t *pkey,
                       int flags,
-                      const char *doctype)
+                      const char *doctype))
 {
   char *signed_digest;
   size_t keysize;
