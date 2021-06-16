3f73ab3f0dbd717c464c8fa899413d6a20df3587
David Goulet <dgoulet@torproject.org>
2021-06-16 10:53:32 -0400

    git: Don't pull github anymore in git-pull-all.sh
    Signed-off-by: David Goulet <dgoulet@torproject.org>

--- scripts/git/git-pull-all.sh
+++ scripts/git/git-pull-all.sh
@@ -159,20 +159,7 @@ function goto_repo
 function fetch_origin
 {
   local cmd="git fetch origin"
-  printf "  %s Fetching origin..." "$MARKER"
-  if [ $DRY_RUN -eq 0 ]; then
-    msg=$( eval "$cmd" 2>&1 )
-    validate_ret $? "$msg"
-  else
-    printf "\\n      %s\\n" "${IWTH}$cmd${CNRM}"
-  fi
-}
-
-# Fetch tor-github pull requests. No arguments.
-function fetch_tor_github
-{
-  local cmd="git fetch tor-github"
-  printf "  %s Fetching tor-github..." "$MARKER"
+  printf "%s Fetching origin..." "$MARKER"
   if [ $DRY_RUN -eq 0 ]; then
     msg=$( eval "$cmd" 2>&1 )
     validate_ret $? "$msg"
@@ -185,7 +172,7 @@ function fetch_tor_github
 function fetch_tor_gitlab
 {
   local cmd="git fetch tor-gitlab"
-  printf "  %s Fetching tor-gitlab..." "$MARKER"
+  printf "%s Fetching tor-gitlab..." "$MARKER"
   if [ $DRY_RUN -eq 0 ]; then
     msg=$( eval "$cmd" 2>&1 )
     validate_ret $? "$msg"
@@ -198,11 +185,10 @@ function fetch_tor_gitlab
 # Entry point #
 ###############
 
-# First, fetch tor-github.
+# Get into our origin repository.
 goto_repo "$ORIGIN_PATH"
-fetch_tor_github
 
-# Then tor-gitlab
+# First, fetch tor-gitlab
 fetch_tor_gitlab
 
 # Then, fetch the origin.
