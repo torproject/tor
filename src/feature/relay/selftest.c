torproject/tor

commit bf10206e9e23ac0ded2cc9727666696ea25d5636

Fix compiler warnings from ubuntu/jammy
Signed-off-by: David Goulet <dgoulet@torproject.org>

src/feature/relay/selftest.c
@@ -254,6 +254,10 @@ router_do_orport_reachability_checks(const routerinfo_t *me,
   if (ei) {
     const char *family_name = fmt_af_family(family);
     const tor_addr_port_t *ap = extend_info_get_orport(ei, family);
+    if (BUG(!ap)) {
+      /* Not much we can do here to recover apart from screaming loudly. */
+      return;
+    }
     log_info(LD_CIRC, "Testing %s of my %s ORPort: %s.",
              !orport_reachable ? "reachability" : "bandwidth",
              family_name, fmt_addrport_ap(ap));

src/feature/dirparse/policy_parse.c
@@ -192,6 +192,10 @@ router_parse_addr_policy_private(directory_token_t *tok)
   uint16_t port_min, port_max;
   addr_policy_t result;
 
+  /* Safeguard: always flag non canonical because it is a stack allocated
+   * object and thus should not be considered a copy stored in a map. */
+  result.is_canonical = 0;
+
   arg = tok->args[0];
   if (strcmpstart(arg, "private"))
     return NULL;

commit 222fbba4ff89dc69aff6dec4b524ca21dc28bf82

relay: Fix memory leak on BUG() code path
Introduced in bf10206e9e23ac0ded2cc9727666696ea25d5636 which is not
released yet thus no changes file.

Found by Coverity with cid #1495786.

Fixes #40532

Signed-off-by: David Goulet <dgoulet@torproject.org>

src/feature/relay/selftest.c
@@ -256,6 +256,7 @@ router_do_orport_reachability_checks(const routerinfo_t *me,
     const tor_addr_port_t *ap = extend_info_get_orport(ei, family);
     if (BUG(!ap)) {
       /* Not much we can do here to recover apart from screaming loudly. */
+      extend_info_free(ei);
       return;
     }
     log_info(LD_CIRC, "Testing %s of my %s ORPort: %s.",