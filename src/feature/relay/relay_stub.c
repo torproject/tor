273227afe1c56ca93ab8c0e3e034bcf28c51e5ef
friendly73 <friendly73@x.x>
2023-04-15 14:48:30 +0000

    Forgot about the stub names

--- src/feature/relay/relay_stub.c
+++ src/feature/relay/relay_stub.c
@@ -22,25 +22,25 @@ const struct subsys_fns_t sys_relay = {
 };
 
 void
-increment_est_intro_action(est_intro_action_t action)
+relay_increment_est_intro_action(est_intro_action_t action)
 {
   (void)action;
 }
 
 void
-increment_est_rend_action(est_rend_action_t action)
+relay_increment_est_rend_action(est_rend_action_t action)
 {
   (void)action;
 }
 
 void
-increment_intro1_action(intro1_action_t action)
+relay_increment_intro1_action(intro1_action_t action)
 {
   (void)action;
 }
 
 void
-increment_rend1_action(rend1_action_t action)
+relay_increment_rend1_action(rend1_action_t action)
 {
   (void)action;
 }
