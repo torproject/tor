2697723cf1485a070776a90d210f872770496ae9
David Goulet <dgoulet@torproject.org>
2023-06-01 09:32:11 -0400

    scripts: Use latest geoip database instead of using location
    Signed-off-by: David Goulet <dgoulet@torproject.org>

--- scripts/maint/geoip/update_geoip.sh
+++ scripts/maint/geoip/update_geoip.sh
@@ -5,7 +5,15 @@ set -e
 DIR=$(cd "$(dirname "$0")" && pwd)
 TMP=$(mktemp -d)
 
-location --quiet update
+DB_PATH="/var/lib/location/database.db"
+
+# In case it exists as a dead symlink.
+if [ -e "$DB_PATH" ]; then
+    unlink "$DB_PATH"
+fi
+
+curl -o "$DB_PATH.xz" "https://location.ipfire.org/databases/1/location.db.xz"
+xz -d "$DB_PATH.xz"
 location dump "$TMP/geoip-dump.txt"
 
 OLDDIR=$(pwd)
